0a9f8048dbba382a41dbae50eb0ccafca8a74efb
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2011-11-18 02:45:05 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@17 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/application/BioPlatPerspective.java
+++ trunk/BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/application/BioPlatPerspective.java
@@ -5,12 +5,12 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class BioPlatPerspective implements IPerspectiveFactory {
 
-    @Override
-    public void createInitialLayout(IPageLayout layout) {
-        layout.addPlaceholder("*progres*", IPageLayout.BOTTOM, 0.22f, layout.getEditorArea());
-    }
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.addView("org.eclipse.ui.views.ProgressView", IPageLayout.BOTTOM, 0.2f, layout.getEditorArea());
+	}
 
-    public static String id() {
-        return "bioplatrcp.perspective"; //$NON-NLS-1$
-    }
+	public static String id() {
+		return "bioplatrcp.perspective"; //$NON-NLS-1$
+	}
 }
