4b21bff8b894b74979e2e022988b4bf5ad8887d7
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2011-11-18 02:48:40 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@23 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/genes/acions/PasteGeneAction.java
+++ trunk/BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/genes/acions/PasteGeneAction.java
@@ -3,6 +3,7 @@ package edu.unlp.medicine.bioplat.rcp.ui.genes.acions;
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -15,59 +16,60 @@ import edu.unlp.medicine.domainLogic.framework.MetaPlat;
 import edu.unlp.medicine.entity.biomarker.Biomarker;
 
 public class PasteGeneAction extends Action {
-    public PasteGeneAction() {
-        setText("Paste!");
-    }
+	public PasteGeneAction() {
+		setText("Paste!");
+	}
 
-    @Override
-    public void run() {
+	@Override
+	public void run() {
 
-        Biomarker b = Models.getInstance().getActiveBiomarker();
-        if (b == null) return;
+		Biomarker b = Models.getInstance().getActiveBiomarker();
+		if (b == null)
+			return;
 
-        mydialog mydialog = new mydialog(null);
-        if (mydialog.open() == Dialog.OK) {
-            for (String id : mydialog.getids()) {
-                b.addGene(MetaPlat.getInstance().getGeneById(id));
-            }
-        }
-    }
+		mydialog mydialog = new mydialog(null);
+		if (mydialog.open() == Dialog.OK)
+			for (String id : mydialog.getids())
+				b.addGene(MetaPlat.getInstance().getGeneById(id));
+
+	}
 }
 
 class mydialog extends Dialog {
 
-    private String separator = " ";
+	private String separator = " ";
 
-    protected mydialog(Shell parentShell) {
-        super(parentShell);
-    }
+	protected mydialog(Shell parentShell) {
+		super(parentShell);
 
-    public String[] getids() {
-        return StringUtils.split(value, separator);
-    }
+	}
 
-    @Override
-    protected void configureShell(Shell newShell) {
-        super.configureShell(newShell);
-        newShell.setText("Pegar los genes separados por '" + separator + "'");
-    }
+	public String[] getids() {
+		return StringUtils.split(value, separator);
+	}
 
-    private String value;
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Pegar los genes separados por '" + separator + "'");
+	}
 
-    @Override
-    protected Control createDialogArea(Composite parent) {
-        Composite result = (Composite) super.createDialogArea(parent);
+	private String value;
 
-        final Text text = new Text(result, SWT.BORDER | SWT.MULTI);
-        text.setSize(100, 100);
-        text.addModifyListener(new ModifyListener() {
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite result = (Composite) super.createDialogArea(parent);
 
-            @Override
-            public void modifyText(ModifyEvent e) {
-                value = text.getText();
-            }
-        });
-        return result;
-    }
+		final Text text = new Text(result, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		text.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+		text.addModifyListener(new ModifyListener() {
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				value = text.getText();
+			}
+		});
+		return result;
+	}
 
 }
