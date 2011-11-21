2853c0cda1691713d8ca93c4891dadbe46fbca27
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2011-11-21 01:55:46 +0000

    nueva definición de menúes
    git-svn-id: https://svn.riouxsvn.com/bioplat04@42 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/application/ApplicationWorkbenchWindowAdvisor.java
+++ trunk/BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/application/ApplicationWorkbenchWindowAdvisor.java
@@ -8,23 +8,24 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        super(configurer);
-    }
+	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
 
-    @Override
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-        return new ApplicationActionBarAdvisor(configurer);
-    }
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
 
-    @Override
-    public void preWindowOpen() {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 500));
-        configurer.setShowCoolBar(false);
-        configurer.setShowStatusLine(false);
-        configurer.setShowPerspectiveBar(false);
-        configurer.setShowProgressIndicator(true);
-        configurer.setTitle("Bio PLat"); //$NON-NLS-1$
-    }
+	@Override
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(400, 500));
+		configurer.setShowMenuBar(true);
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+		configurer.setShowPerspectiveBar(false);
+		configurer.setShowProgressIndicator(true);
+		configurer.setTitle("Bio PLat"); //$NON-NLS-1$
+	}
 }
