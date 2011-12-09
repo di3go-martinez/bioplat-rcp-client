2b971244a5821d41ed7fc27c410a8eb0041549d3
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2011-12-09 23:29:21 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@67 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/editor/input/DefaultEditorInput.java
+++ BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/editor/input/DefaultEditorInput.java
@@ -1,7 +1,18 @@
 package edu.unlp.medicine.bioplat.rcp.editor.input;
 
+import edu.unlp.medicine.bioplat.rcp.utils.EditorInputFactory;
 
+/**
+ * 
+ * @author diego
+ * 
+ * @param <T>
+ * 
+ * @deprecated no usar directamente, usar la factory
+ * @see EditorInputFactory
+ */
 
+@Deprecated
 public class DefaultEditorInput<T> extends AbstractEditorInput<T> {
 
 	public DefaultEditorInput(T object) {
