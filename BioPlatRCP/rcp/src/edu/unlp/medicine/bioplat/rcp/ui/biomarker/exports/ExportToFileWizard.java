bf9d4b13213f2ca9b7b19ff5665abaa47ebf5274
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2011-12-09 23:31:52 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@72 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/exports/ExportToFileWizard.java
+++ BioPlatRCP/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/exports/ExportToFileWizard.java
@@ -1,23 +1,29 @@
 package edu.unlp.medicine.bioplat.rcp.ui.biomarker.exports;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.observable.ChangeEvent;
+import org.eclipse.core.databinding.observable.IChangeListener;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.jface.databinding.wizard.WizardPageSupport;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IExportWizard;
 import org.eclipse.ui.IWorkbench;
 
 import edu.unlp.medicine.bioplat.rcp.ui.utils.Models;
+import edu.unlp.medicine.bioplat.rcp.ui.utils.databinding.validators.FilePathValidator;
 import edu.unlp.medicine.domainLogic.ext.metasignatureCommands.ExportGenesToCSVFileCommand;
 import edu.unlp.medicine.entity.biomarker.Biomarker;
 
@@ -31,15 +37,45 @@ import edu.unlp.medicine.entity.biomarker.Biomarker;
 public class ExportToFileWizard extends Wizard implements IExportWizard {
 
 	public ExportToFileWizard() {
-		// TODO Auto-generated constructor stub
 	}
 
-	private String text;
 	private Map<String, String> parameters = new HashMap<String, String>();
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		addPage(createWizardPage());
+	}
+
+	private class WizardModel {
+		IObservableValue separator = new WritableValue(".", String.class);
+
+		IObservableValue path = new WritableValue(null, String.class);
+		{
+			separator.addChangeListener(new IChangeListener() {
+
+				@Override
+				public void handleChange(ChangeEvent event) {
+					parameters.put(ExportGenesToCSVFileCommand.SEPARATOR, separator.getValue().toString());
+				}
+			});
+
+			path.addChangeListener(new IChangeListener() {
+
+				@Override
+				public void handleChange(ChangeEvent event) {
+					parameters.put(ExportGenesToCSVFileCommand.PATH_OF_THE_OUTPUT_FILE, path.getValue().toString());
+				}
+			});
+			// le seteo valores por default
+			separator.setValue(",");
+			path.setValue("c:/tmp/biomarker");
+		}
+	}
+
+	private WizardModel model = new WizardModel();
+
+	private WizardModel getModel() {
+		return model;
 	}
 
 	private WizardPage createWizardPage() {
@@ -51,34 +87,25 @@ public class ExportToFileWizard extends Wizard implements IExportWizard {
 			@Override
 			public void createControl(Composite parent) {
 
-				Composite c = new Composite(parent, SWT.BORDER);
-				c.setLayout(GridLayoutFactory.fillDefaults().create());
+				DataBindingContext dbc = new DataBindingContext();
+				WizardPageSupport.create(this, dbc);
 
-				new CLabel(c, SWT.BOLD).setText("separador:");
+				Composite c = new Composite(parent, SWT.BORDER);
+				// c.setLayout(GridLayoutFactory.fillDefaults().numColumns(2).create());
+
+				new CLabel(c, SWT.BOLD).setText("Separador:");
 
 				Text separatorHolder = new Text(c, SWT.BORDER);
-				separatorHolder.addModifyListener(new ModifyListener() {
 
-					@Override
-					public void modifyText(ModifyEvent e) {
-						parameters.put(ExportGenesToCSVFileCommand.SEPARATOR, ((Text) e.getSource()).getText());
-					}
-				});
-				separatorHolder.setText(",");
+				dbc.bindValue(SWTObservables.observeText(separatorHolder, SWT.Modify), getModel().separator);
 
-				new CLabel(c, SWT.BOLD).setText("archivo:");
+				new CLabel(c, SWT.BOLD).setText("Archivo:");
 				Text fileHolder = new Text(c, SWT.BORDER);
-				fileHolder.addModifyListener(new ModifyListener() {
 
-					@Override
-					public void modifyText(ModifyEvent e) {
-						text = ((Text) e.getSource()).getText();
-						File file = new File(text);
-						setPageComplete(!file.isDirectory());
-						parameters.put(ExportGenesToCSVFileCommand.PATH_OF_THE_OUTPUT_FILE, text);
-					}
-				});
-				fileHolder.setText("c:/tmp/b");
+				dbc.bindValue(SWTObservables.observeText(fileHolder, SWT.Modify), getModel().path,//
+						new UpdateValueStrategy().setAfterConvertValidator(new FilePathValidator()), null);
+
+				GridLayoutFactory.swtDefaults().numColumns(2).generateLayout(c);
 				setControl(c);
 			}
 
