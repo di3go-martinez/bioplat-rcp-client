51693569a349925a97101a9525997821e1163805
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-02-16 05:24:07 +0000

    TODO!
    git-svn-id: https://svn.riouxsvn.com/bioplat04@135 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/contributor/ExperimetEditorActionBarContributor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/contributor/ExperimetEditorActionBarContributor.java
@@ -1,11 +1,17 @@
 package edu.unlp.medicine.bioplat.rcp.ui.experiment.contributor;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IEditorPart;
 
 import edu.unlp.medicine.bioplat.rcp.ui.entities.editors.contributors.AbstractEditorActionBarContributor;
+import edu.unlp.medicine.bioplat.rcp.ui.experiment.editors.ExperimentEditor;
+import edu.unlp.medicine.bioplat.rcp.ui.genes.GeneSearch;
 import edu.unlp.medicine.entity.experiment.Experiment;
 
 public class ExperimetEditorActionBarContributor extends AbstractEditorActionBarContributor<Experiment> {
+
+	private ExperimentEditor editor;
 
 	@Override
 	protected Class<?> getSelectionType() {
@@ -15,6 +21,31 @@ public class ExperimetEditorActionBarContributor extends AbstractEditorActionBar
 	@Override
 	protected void populateMenu(IMenuManager menu) {
 
+//		menu.add(new Action() {
+//			@Override
+//			public void run() {
+//				doRun();
+//			}
+//
+//			@Override
+//			public String getText() {
+//				return "find...";
+//			}
+//		});
+
 	}
 
+	@Override
+	public void setActiveEditor(IEditorPart targetEditor) {
+		this.editor = (ExperimentEditor) targetEditor;
+	}
+
+	private void doRun() {
+		GeneSearch gs = GeneSearch.createDialog();
+
+		if (gs.open() == GeneSearch.OK) {
+			editor.showGene(gs.selectedGene());
+		}
+
+	}
 }
