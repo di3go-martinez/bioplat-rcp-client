45cbdb11c8a563ef0326e5f16f6e116a1b930f86
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-02-19 18:03:23 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@149 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/genes/view/GeneViewPart.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/genes/view/GeneViewPart.java
@@ -2,10 +2,11 @@ package edu.unlp.medicine.bioplat.rcp.ui.genes.view;
 
 import java.util.List;
 
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISelectionService;
@@ -14,14 +15,15 @@ import org.eclipse.ui.part.ViewPart;
 
 import com.google.common.collect.Lists;
 
-import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.ColumnBuilder;
-import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableBuilder;
-import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableReference;
+import edu.unlp.medicine.bioplat.rcp.core.selections.MultipleSelection;
+import edu.unlp.medicine.bioplat.rcp.editor.Constants;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widget;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
-import edu.unlp.medicine.entity.biomarker.Biomarker;
+import edu.unlp.medicine.entity.gene.Gene;
 
 public class GeneViewPart extends ViewPart {
+
+	private static final String REST_URL_NCBI = "http://www.ncbi.nlm.nih.gov/gene/?term=";
 
 	public static String id() {
 		return "edu.medicine.bioplat.rcp.gene.view";
@@ -34,21 +36,37 @@ public class GeneViewPart extends ViewPart {
 
 		getSelectionService().addSelectionListener(listener = new ISelectionListener() {
 
+			private Gene oldGene;
+
 			@Override
 			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-				Biomarker b = null;
+				Gene gene = null;
 
 				if (!isMySelf(part) && isValidSelection(selection))
-					b = (Biomarker) ((StructuredSelection) selection).getFirstElement();
+					gene = getGeneSelection(selection);
 
-				createComposite(parent, b);
+				if (gene != null && (oldGene == null || !gene.equals(oldGene)))
+					updateComposite(parent, gene);
 
+				oldGene = gene;
+
+			}
+
+			private Gene getGeneSelection(ISelection selection) {
+				Object result = null;
+				if (selection instanceof MultipleSelection)
+					result = ((MultipleSelection) selection).get(Constants.GENES).getFirstElement();
+
+				// result ((StructuredSelection) selection).getFirstElement();
+				return (Gene) result;
 			}
 
 			private boolean isValidSelection(ISelection s) {
 				return !s.isEmpty() && //
-						s instanceof StructuredSelection && //
-						((StructuredSelection) s).getFirstElement() instanceof Biomarker;
+						s instanceof MultipleSelection //
+				// && ((StructuredSelection) s).getFirstElement()
+				// instanceof Gene
+				;
 			}
 
 			private boolean isMySelf(IWorkbenchPart part) {
@@ -71,9 +89,9 @@ public class GeneViewPart extends ViewPart {
 
 	private Composite c;
 	private List<Widget> ws = Lists.newArrayList();
-	private TableReference tr;
+	private Browser browser;
 
-	private void createComposite(Composite parent, Biomarker input) {
+	private void updateComposite(Composite parent, Gene gene) {
 
 		// c.dispose();
 		// c = null;
@@ -84,30 +102,36 @@ public class GeneViewPart extends ViewPart {
 		if (c == null) {
 			c = Widgets.createDefaultContainer(parent);
 			c.setLayout(GridLayoutFactory.fillDefaults().numColumns(2).create());
+
 		}
-		if (input != null) {
+
+		c.setVisible(gene != null);
+
+		if (gene != null) {
 			if (!ws.isEmpty()) {
 				for (Widget w : ws)
-					w.retarget(input);
-				tr.refresh();
-			} else {
-				ws.add(Widgets.createTextWithLabel(c, "nombre", input, "name", true));
-				tr = TableBuilder.create(c).model(input, "genes").//
-						addColumn(ColumnBuilder.create().property("name").title("Nombre")).//
-						addColumn(ColumnBuilder.create().property("entrezId").title("ID").labelprovider(new ColumnLabelProvider() {
-							@Override
-							public String getText(Object element) {
-								return "asdfadsf";
-							}
-						})).//
-						build();
+					w.retarget(gene);
 
+				browser.setUrl(makeRestUrl(gene));
+			} else {
+				ws.add(Widgets.createTextWithLabel(c, "nombre", gene, "name", true));
+				browser = new Browser(c, SWT.BORDER);
+				browser.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).span(2, 1).create());
+				browser.setUrl(makeRestUrl(gene));
 			}
 		}
 
+		// FIXME no se repinta bien la primera vez que aparece si no se crea el
+		// composite container, se fuerza con el -1
+		parent.setSize(parent.getSize().x, parent.getSize().y);
 		parent.layout();
 		parent.redraw();
 		parent.update();
+
+	}
+
+	private String makeRestUrl(Gene input) {
+		return REST_URL_NCBI + input.getEntrezId();
 	}
 
 	@Override
