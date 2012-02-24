08d78eb0d70f16d2d26e853d2c70e600aa230282
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-02-24 06:47:48 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@186 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/utils/wizards/WizardModel.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/utils/wizards/WizardModel.java
@@ -9,6 +9,7 @@ import com.google.common.collect.Maps;
 public class WizardModel {
 
 	private Map<String, IObservableValue> values = Maps.newHashMap();
+	private Map<String, Object> commonValues = Maps.newHashMap();
 
 	public WizardModel add(String key, IObservableValue value) {
 		values.put(key, value);
@@ -21,6 +22,25 @@ public class WizardModel {
 
 	public IObservableValue valueHolder(String key) {
 		return values.get(key);
+	}
+
+	// TODO analizar...
+	public WizardModel set(String key, Object value) {
+		commonValues.put(key, value);
+		return this;
+	}
+
+	// TODO analizar...
+	public Object get(String key) {
+		return get(key, null);
+	}
+
+	// TODO analizar...
+	public Object get(String key, Object def) {
+		Object result = commonValues.get(key);
+		if (result == null)
+			result = def;
+		return result;
 	}
 
 }
