44664b407a7f23074153bf44cf82263e945a3891
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-02-25 16:45:39 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@189 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/ApplicationWorkbenchAdvisor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/ApplicationWorkbenchAdvisor.java
@@ -5,30 +5,32 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
+import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
+import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
+
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-
-    @Override
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
 
 	@Override
-    public String getInitialWindowPerspectiveId() {
-        return BioPlatPerspective.id();
+	public String getInitialWindowPerspectiveId() {
+		return BioPlatPerspective.id();
 	}
-	
+
 	@Override
 	public void initialize(IWorkbenchConfigurer configurer) {
 		super.initialize(configurer);
 		configurer.setSaveAndRestore(true);
 	}
-	
-    // TODO eventualmente manejar un error
+
+	// TODO eventualmente manejar un error
 	@Override
 	public void eventLoopException(Throwable exception) {
-        System.out.println(exception.getMessage());
-        // getWorkbenchConfigurer().emergencyClose();
-        super.eventLoopException(exception);
+		Message m = Message.error("Error inesperado", exception);
+		MessageManager.INSTANCE.add(m);
+		super.eventLoopException(exception);
 	}
 }
