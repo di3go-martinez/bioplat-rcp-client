e24f8b126de1e4b0e71df8daba0dc1ff2362276b
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-02-25 16:54:56 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@197 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

---
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/EditorsId.java
@@ -0,0 +1,10 @@
+package edu.unlp.medicine.bioplat.rcp.ui.entities;
+
+import edu.unlp.medicine.bioplat.rcp.ui.biomarker.editors.MultiPageBiomarkerEditor;
+
+public class EditorsId {
+
+	public static String biomarkerEditorId() {
+		return MultiPageBiomarkerEditor.id();
+	}
+}
