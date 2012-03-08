d20e49f8e3b09e0b69d2ed7a99e59c46aab8e838
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-03-08 04:24:52 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@245 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/editors/StatisticalDataEditor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/editors/StatisticalDataEditor.java
@@ -1,14 +1,15 @@
 package edu.unlp.medicine.bioplat.rcp.ui.experiment.editors;
 
-import java.util.concurrent.Callable;
-
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.events.ExpansionAdapter;
 import org.eclipse.ui.forms.events.ExpansionEvent;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
 
 import edu.unlp.medicine.bioplat.rcp.editor.AbstractEditorPart;
 import edu.unlp.medicine.entity.experiment.ExperimentAppliedToAMetasignature;
@@ -24,52 +25,80 @@ public class StatisticalDataEditor extends AbstractEditorPart<ExperimentAppliedT
 	protected void doCreatePartControl(final Composite parent) {
 		toolkit = new FormToolkit(parent.getDisplay());
 
-		Composite container = toolkit.createComposite(parent);
-		container.setLayout(GridLayoutFactory.fillDefaults().margins(10, 10).create());
+		ScrolledForm container = toolkit.createScrolledForm(parent);
+		// container.setLayout(GridLayoutFactory.fillDefaults().create());
+		container.getBody().setLayout(GridLayoutFactory.fillDefaults().margins(10, 10).create());
 
 		final ExperimentAppliedToAMetasignature experiment = model();
 
-		createExpandable(container, experiment, "Script For Dooing The Cluster", new Callable<String>() {
+		createExpandable(container, experiment, "Script For Dooing The Cluster", new CompositeGenerator() {
 
 			@Override
-			public String call() throws Exception {
-				return experiment.getScriptForDoingTheCluster();
+			public Composite generate(Composite parent) throws Exception {
+				ScrolledForm result = toolkit.createScrolledForm(parent);
+				result.setLayout(GridLayoutFactory.fillDefaults().create());
+
+				Composite c = result.getBody();
+				c.setLayout(GridLayoutFactory.fillDefaults().create());
+
+				Text t = new Text(c, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+				t.setText(experiment.getScriptForDoingTheCluster());
+
+				return result;
 			}
 		});
 
-		createExpandable(container, experiment, "LogRankTestChiSqured", new Callable<String>() {
+		createExpandable(container, experiment, "LogRankTestChiSqured", new CompositeGenerator() {
 
 			@Override
-			public String call() throws Exception {
-				return experiment.generateScript4LogRankTestChiSqured();
+			public Composite generate(Composite parent) throws Exception {
+
+				ScrolledForm result = toolkit.createScrolledForm(parent);
+				result.setLayout(GridLayoutFactory.fillDefaults().create());
+
+				Composite c = result.getBody();
+				c.setLayout(GridLayoutFactory.fillDefaults().create());
+				String script;
+				try {
+					script = experiment.generateScript4LogRankTestChiSqured();
+				} catch (Exception e) {
+					script = "Couldn't generate the script...";
+				}
+				Text t = new Text(c, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+				t.setText(script);
+
+				return result;
+
 			}
 		});
 
 	}
 
-	private void createExpandable(final Composite parent, ExperimentAppliedToAMetasignature experiment, String title, Callable<String> scriptGenerator) {
-		final ExpandableComposite ec = toolkit.createExpandableComposite(parent, ExpandableComposite.TWISTIE | ExpandableComposite.CLIENT_INDENT/*
-																																				 * TREE_NODE
-																																				 */);
-
+	private void createExpandable(final ScrolledForm parent, ExperimentAppliedToAMetasignature experiment, String title, CompositeGenerator compositeGenerator) {
+		final ExpandableComposite ec = toolkit.createExpandableComposite(parent.getBody(), ExpandableComposite.TWISTIE | ExpandableComposite.CLIENT_INDENT/*
+																																						 * TREE_NODE
+																																						 */);
 		ec.setText(title);
 
-		Composite client = toolkit.createComposite(ec);
-		client.setLayout(GridLayoutFactory.fillDefaults().create());
-
-		String $script = "Couldn't generate the script...";
+		Composite client;
+		// = toolkit.createComposite(ec);
+		// client.setLayout(GridLayoutFactory.fillDefaults().create());
+		//
+		// String $script = "Couldn't generate the script...";
 		try {
-			$script = scriptGenerator.call();
+			client = compositeGenerator.generate(ec);
 		} catch (Exception e) {
 			e.printStackTrace();
+			client = toolkit.createComposite(ec);
+			new Label(client, SWT.NONE).setText("Couldn't generate....");
 		}
-		toolkit.createLabel(client, $script, SWT.WRAP);
+		// toolkit.createLabel(client, $script, SWT.WRAP);
 
 		ec.setClient(client);
 		ec.addExpansionListener(new ExpansionAdapter() {
 			@Override
 			public void expansionStateChanged(ExpansionEvent e) {
-				parent.layout(true);
+				parent.reflow(true);
 			}
 		});
 	}
@@ -79,4 +108,8 @@ public class StatisticalDataEditor extends AbstractEditorPart<ExperimentAppliedT
 		toolkit.dispose();
 		super.dispose();
 	}
+}
+
+interface CompositeGenerator {
+	Composite generate(Composite parent) throws Exception;
 }
