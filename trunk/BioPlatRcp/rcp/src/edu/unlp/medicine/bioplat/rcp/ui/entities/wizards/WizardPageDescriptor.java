5ae369bdc289b6905932f3daae298e7f9091fa59
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-03-28 04:06:03 +0000

    javadoc
    git-svn-id: https://svn.riouxsvn.com/bioplat04@280 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/wizards/WizardPageDescriptor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/wizards/WizardPageDescriptor.java
@@ -1,6 +1,5 @@
 package edu.unlp.medicine.bioplat.rcp.ui.entities.wizards;
 
-
 import javax.annotation.Nullable;
 
 import org.eclipse.core.databinding.DataBindingContext;
@@ -21,6 +20,24 @@ public abstract class WizardPageDescriptor {
 		return name;
 	};
 
+	/**
+	 * 
+	 * Create el composite para una página del wizard <code>wizardPage</code>
+	 * 
+	 * @param wizardPage
+	 *            es la página para la cual se creará el composite. Notar que
+	 *            puede ser null
+	 * @param parent
+	 *            donde se crearán los controles
+	 * @param dbc
+	 *            Es el contexto actual para la configuración de bindings entre
+	 *            el model y la vista
+	 * @param wmodel
+	 *            es el model del wizard, donde se registran las propiedades que
+	 *            luego se accederán, en principio, desde el wizard "contenedor"
+	 *            en el proceso de "finish()"
+	 * @return el composite creado
+	 */
 	public abstract Composite create(@Nullable WizardPage wizardPage, Composite parent, DataBindingContext dbc, WizardModel wmodel);
 
 	@Override
