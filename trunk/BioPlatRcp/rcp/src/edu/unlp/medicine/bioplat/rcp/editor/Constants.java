f64f074d9540ab1ef07cdbd504d78d4d1b74b3de
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-04-03 05:33:57 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@287 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/editor/Constants.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/editor/Constants.java
@@ -3,8 +3,9 @@ package edu.unlp.medicine.bioplat.rcp.editor;
 public interface Constants {
 
 	String DEFAULT = "default key";
-	String GENES = "genes keys";
 	String BIOMARKER = "biomarker key";
 	Object MODEL = "model";
+	String GENES = "genes key";
+	String SELECTED_GENES = "selected genes key";
 
 }
