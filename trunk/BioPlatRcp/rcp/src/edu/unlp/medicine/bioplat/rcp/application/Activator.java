45f591216aacb20d7f79401879d9a4dcf37db620
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-04-07 21:40:19 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@305 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/Activator.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/Activator.java
@@ -5,6 +5,8 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class Activator extends AbstractUIPlugin {
 
+	private static final String RESOURCES_ICONS = "resources/icons/";
+
 	public static String id() {
 		return "edu.medicine.bioplat.rcp";
 	}
@@ -14,6 +16,9 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	public static ImageDescriptor imageDescriptorFromPlugin(String imageFilePath) {
+		if (!imageFilePath.contains(RESOURCES_ICONS))
+			imageFilePath = RESOURCES_ICONS + imageFilePath;
+
 		ImageDescriptor id = imageDescriptorFromPlugin(id(), imageFilePath);
 		if (id == null)
 			id = imageDescriptorFromPlugin("resources/icons/default.png");
