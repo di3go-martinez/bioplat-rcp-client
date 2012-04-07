cf55aa6ca6b5f4abb167247b333e720b36a27536
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-04-07 21:42:15 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@307 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/editors/contributors/AbstractActionContribution.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/editors/contributors/AbstractActionContribution.java
@@ -2,11 +2,69 @@ package edu.unlp.medicine.bioplat.rcp.ui.entities.editors.contributors;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWindowListener;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
 import edu.unlp.medicine.bioplat.rcp.editor.ModelProvider;
+import edu.unlp.medicine.bioplat.rcp.utils.Holder;
+import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 import edu.unlp.medicine.entity.generic.AbstractEntity;
 
 public abstract class AbstractActionContribution<T extends AbstractEntity> extends Action implements ActionContribution<T> {
+
+	// TODO revisar bien esto.... a veces tira error
+	private IEditorPart activeEditor;
+
+	public AbstractActionContribution() {
+		PlatformUI.getWorkbench().addWindowListener(new IWindowListener() {
+
+			@Override
+			public void windowOpened(IWorkbenchWindow window) {
+				// TODO revisar
+				windowActivated(window);
+			}
+
+			@Override
+			public void windowDeactivated(IWorkbenchWindow window) {
+
+			}
+
+			@Override
+			public void windowClosed(IWorkbenchWindow window) {
+				// PlatformUI.getWorkbench().removeWindowListener(this);
+			}
+
+			@Override
+			public void windowActivated(IWorkbenchWindow window) {
+				activeEditor = editor(window);
+			}
+
+			private IEditorPart editor(IWorkbenchWindow window) {
+				return activeEditor = window.getActivePage().getActiveEditor();
+			}
+		});
+	}
+
+	protected <T extends ISelection> T getSelection() {
+		final Holder<ISelection> sh = Holder.create();
+		PlatformUIUtils.findDisplay().syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				sh.hold(getCurrentEditor().getSite().getSelectionProvider().getSelection());
+			}
+		});
+
+		T ms = (T) sh.value();
+		return ms;
+	}
+
+	protected IEditorPart getCurrentEditor() {
+		return activeEditor;
+	}
 
 	private ModelProvider<T> modelProvider;
 
