58c56dd070204744d6571eafaefcdd6c58a07841
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-04-16 05:23:20 +0000

    Moviendo acciones al poc agregando logging agregando la acción de aplicar logaritmos
    git-svn-id: https://svn.riouxsvn.com/bioplat04@327 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/FreeGeneInputActionContribution.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/FreeGeneInputActionContribution.java
@@ -3,6 +3,9 @@ package edu.unlp.medicine.bioplat.rcp.ui.experiment.actions.contributions;
 import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.unlp.medicine.bioplat.rcp.core.selections.MultipleSelection;
 import edu.unlp.medicine.bioplat.rcp.editor.Constants;
 import edu.unlp.medicine.bioplat.rcp.ui.entities.editors.contributors.AbstractActionContribution;
@@ -13,34 +16,40 @@ import edu.unlp.medicine.entity.experiment.Experiment;
 import edu.unlp.medicine.entity.gene.Gene;
 
 public abstract class FreeGeneInputActionContribution extends AbstractActionContribution<Experiment> {
+	private static Logger logger = LoggerFactory.getLogger(FreeGeneInputActionContribution.class);
 
 	private static final List<Gene> EMPTY = Collections.emptyList();
 
 	@Override
 	public void run() {
 
-		final Holder<List<Gene>> holder = Holder.create(EMPTY);
+		// FIXME Chatcheo el nullpointer porque pasa a veces...
+		try {
+			final Holder<List<Gene>> holder = Holder.create(EMPTY);
 
-		PlatformUIUtils.findDisplay().syncExec(new Runnable() {
-			@Override
-			public void run() {
-				GenesInputDialog agd = new GenesInputDialog().genes(selectedGenes());
-				if (agd.accepted()) {
-					holder.hold(agd.genes());
+			PlatformUIUtils.findDisplay().syncExec(new Runnable() {
+				@Override
+				public void run() {
+
+					GenesInputDialog agd = new GenesInputDialog().genes(selectedGenes());
+					if (agd.accepted())
+						holder.hold(agd.genes());
+
 				}
-			}
 
-			private List<Gene> selectedGenes() {
-				MultipleSelection ms = getSelection();
-				return ms.get(Constants.SELECTED_GENES).toList();
-			}
+				private List<Gene> selectedGenes() {
+					MultipleSelection ms = getSelection();
+					return ms.get(Constants.SELECTED_GENES).toList();
+				}
 
-		});
+			});
 
-		final List<Gene> value = holder.value();
-		if (!value.isEmpty())
-			executeOn(value);
-
+			final List<Gene> value = holder.value();
+			if (!value.isEmpty())
+				executeOn(value);
+		} catch (NullPointerException npe) {
+			logger.warn("nullpointer en la acción, catcheado apropósito");
+		}
 	}
 
 	/**
