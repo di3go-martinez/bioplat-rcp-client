ddb723b7faca1a7dc505f57eb5cfb449a2c4f085
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-04-23 05:55:14 +0000

    Wizard Utils
    git-svn-id: https://svn.riouxsvn.com/bioplat04@357 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/utils/wizards/WizardModel.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/utils/wizards/WizardModel.java
@@ -9,6 +9,8 @@ import org.eclipse.core.databinding.observable.value.WritableValue;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+import edu.unlp.medicine.bioplat.rcp.utils.Holder;
+
 public class WizardModel {
 
 	private Map<String, IObservableValue> values = Maps.newHashMap();
@@ -19,6 +21,10 @@ public class WizardModel {
 		return this;
 	}
 
+	public <T> WizardModel add(String key, Class<T> type, T initialValue) {
+		return add(key, new WritableValue(initialValue, type));
+	}
+
 	public WizardModel add(String key) {
 		return add(key, new WritableValue());
 	}
@@ -26,8 +32,22 @@ public class WizardModel {
 	public <T> T value(final String key) {
 		IObservableValue ov = valueHolder(key);
 		if (ov != null)
-			return (T) ov.getValue();
+			return (T) ov.getValue();// TODO ensureDoingInRealm(ov);
 		return (T) commonValues.get(key);
+	}
+
+	private <T> T ensureDoingInRealm(final IObservableValue ov) {
+		final Holder<T> holder = Holder.create();
+
+		ov.getRealm().exec(new Runnable() {
+
+			@Override
+			public void run() {
+				holder.hold((T) ov.getValue());
+			}
+		});
+
+		return holder.value();
 	}
 
 	public List<String> keys() {
