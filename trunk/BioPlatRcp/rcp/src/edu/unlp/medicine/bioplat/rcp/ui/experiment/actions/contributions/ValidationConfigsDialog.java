2bb7a03febacdef8c556c5fc314a6b326717e6b1
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-04-23 05:51:32 +0000

    Refactor, saco el wizard a una clase
    git-svn-id: https://svn.riouxsvn.com/bioplat04@353 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/ValidationConfigsDialog.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/ValidationConfigsDialog.java
@@ -1,13 +1,13 @@
 package edu.unlp.medicine.bioplat.rcp.ui.experiment.actions.contributions;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
-import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -17,34 +17,18 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 
-import com.google.common.base.Function;
-import com.google.common.collect.DiscreteDomains;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Ranges;
 
-import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.AbstractWizard;
-import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.PagesDescriptors;
-import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.WizardPageDescriptor;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.ColumnBuilder;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableBuilder;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableReference;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
-import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
-import edu.unlp.medicine.bioplat.rcp.utils.wizards.WizardModel;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
 import edu.unlp.medicine.domainLogic.ext.metasignatureCommands.ApplyExperimentsOnMetasignatureCommand;
-import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.experimentDescriptor.AbstractExperimentDescriptor;
-import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.experimentDescriptor.FromMemoryExperimentDescriptor;
-import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.significanceTest.AttributeTypeEnum;
-import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.significanceTest.IStatisticsSignificanceTest;
 import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.significanceTest.ValidationConfig;
 import edu.unlp.medicine.entity.biomarker.Biomarker;
-import edu.unlp.medicine.entity.experiment.Experiment;
-import edu.unlp.medicine.entity.experiment.exception.ExperimentBuildingException;
-import edu.unlp.medicine.utils.monitor.Monitor;
 
 public class ValidationConfigsDialog extends TitleAreaDialog {
 
@@ -53,9 +37,10 @@ public class ValidationConfigsDialog extends TitleAreaDialog {
 		this.biomarker = biomarker;
 
 		// TODO mejorar
-		experimentsWizard = createExperimentSelectorWizard();
+		experimentsWizard = createExperimentSelectorWizard(biomarker);
 		// TODO conseguir la selección actual
-		experimentsWizard.init(PlatformUI.getWorkbench(), StructuredSelection.EMPTY);
+		// experimentsWizard.init(PlatformUI.getWorkbench(),
+		// StructuredSelection.EMPTY);
 
 	}
 
@@ -112,10 +97,12 @@ public class ValidationConfigsDialog extends TitleAreaDialog {
 		add.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				wd = new WizardDialog(PlatformUIUtils.findShell(), experimentsWizard);
-				wd.setPageSize(400, 400);
-				wd.setTitle("Agregar Validation Configs");
-				wd.open();
+				// wd = new WizardDialog(PlatformUIUtils.findShell(),
+				// experimentsWizard);
+				// wd.setPageSize(400, 400);
+				// wd.setTitle("Agregar Validation Configs");
+				// wd.open();
+				experimentsWizard.open();
 			}
 		});
 		super.createButtonsForButtonBar(c);
@@ -124,129 +111,49 @@ public class ValidationConfigsDialog extends TitleAreaDialog {
 
 	@Override
 	protected void okPressed() {
-		int count = 0;
-		for (ApplyExperimentsOnMetasignatureCommand command : applyCommands) {
-			try {
-				command.execute();
-				count++;
-			} catch (Exception e) {
-				MessageManager.INSTANCE.add(Message.error("Falló la aplicación del experimento", e));
-			}
-		}
-		String s = (count == 1) ? "" : "s";
-		MessageManager.INSTANCE.add(Message.info(count + " comando" + s + " aplicado" + s + " exitósamente"));
 		close();
+		Job j = new Job("Applying Validation Configs") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				monitor.beginTask("", experimentsWizard.commands2apply().size());
+				int count = 0;
+				for (ApplyExperimentsOnMetasignatureCommand command : experimentsWizard.commands2apply()) {
+					try {
+						command.execute();
+						count++;
+					} catch (Exception e) {
+						MessageManager.INSTANCE.add(Message.error("Falló la aplicación del experimento", e));
+					}
+					monitor.worked(1);
+				}
+				String s = (count == 1) ? "" : "s";
+				MessageManager.INSTANCE.add(Message.info(count + " comando" + s + " aplicado" + s + " exitósamente"));
+				monitor.done();
+				return Status.OK_STATUS;
+			}
+		};
+		j.setUser(true);
+		j.setPriority(Job.LONG);
+		j.schedule();
+
 	}
 
 	private Biomarker biomarker;
-	private AbstractWizard<?> experimentsWizard;
+	private ValidationConfigWizard experimentsWizard;
 	private WizardDialog wd;
 	private TableReference tr;
 
-	private Biomarker findBiomarker() {
-		return biomarker;
-	}
-
-	private List<ApplyExperimentsOnMetasignatureCommand> applyCommands = Lists.newArrayList();
-
-	private AbstractWizard<?> createExperimentSelectorWizard() {
-		final AbstractWizard<List<AbstractExperimentDescriptor>> w = new AbstractWizard<List<AbstractExperimentDescriptor>>() {
-
+	private ValidationConfigWizard createExperimentSelectorWizard(Biomarker biomarker) {
+		return new ValidationConfigWizard(biomarker) {
 			@Override
-			public List<AbstractExperimentDescriptor> backgroundProcess(Monitor feedback) throws ExperimentBuildingException {
-				List<Experiment> experiments = ((List<Experiment>) wizardModel().value(PagesDescriptors.SELECTED));
-
-				return Lists.transform(experiments, new Function<Experiment, AbstractExperimentDescriptor>() {
-
-					@Override
-					public AbstractExperimentDescriptor apply(Experiment input) {
-						return new FromMemoryExperimentDescriptor(input);
-					}
-				});
-				// TODO hacer para los otros sources (archivo, InSilicoDB, etc)
+			protected void register(ValidationConfig validationConfig) {
+				data.add(validationConfig);
 			}
 
 			@Override
-			protected void doInUI(List<AbstractExperimentDescriptor> appliedExperiments) throws Exception {
-
-				boolean shouldGenerateCluster = wizardModel().value(PagesDescriptors.GENERATE_CLUSTER_CALCULATE_BIOLOGICAL_VALUE);
-				// numberOfCluster puede ser un rango
-				String numberOfClusters = wizardModel().value(PagesDescriptors.NUMBER_OF_CLUSTERS);
-				String attributeNameToValidation = wizardModel().value(PagesDescriptors.ATTRIBUTE_NAME_TO_VALIDATION);
-				String secondAttributeNameToDoTheValidation = wizardModel().value(PagesDescriptors.SECOND_ATTRIBUTE_NAME_TO_VALIDATION);
-				IStatisticsSignificanceTest statisticsSignificanceTest = wizardModel().value(PagesDescriptors.STATISTICAL_TEST_VALUE);
-				int numberOfTimesToRepeatTheCluster = wizardModel().value(PagesDescriptors.TIMES_TO_REPEAT_CLUSTERING);
-				boolean removeInBiomarkerTheGenesThatAreNotInTheExperiment = wizardModel().value(PagesDescriptors.REMOVE_GENES_IN_BIOMARKER);
-
-				for (AbstractExperimentDescriptor aed : appliedExperiments) {
-
-					for (Integer clusters : calculateRange(numberOfClusters)) {
-						final ValidationConfig validationConfig = new ValidationConfig(aed, shouldGenerateCluster, clusters, attributeNameToValidation, secondAttributeNameToDoTheValidation, statisticsSignificanceTest, numberOfTimesToRepeatTheCluster, removeInBiomarkerTheGenesThatAreNotInTheExperiment);
-
-						applyCommands.add(new ApplyExperimentsOnMetasignatureCommand(findBiomarker(), Lists.newArrayList(validationConfig)));
-
-						// FIXME hacer un poquito más generico con una
-						// interface MultipageEditor#addPage(Editor, Input,
-						// [title])...
-						// MultiPageBiomarkerEditor multipleEditor =
-						// (MultiPageBiomarkerEditor)
-						// PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-						//
-						// final ExperimentAppliedToAMetasignature
-						// appliedExperiment
-						// = findModel().getExperimentApplied(ae);
-						// multipleEditor.addEditorPage(new ExperimentEditor(),
-						// EditorInputFactory.createDefaultEditorInput(appliedExperiment));
-
-						data.add(validationConfig);
-					}
-					tr.refresh();
-				}
-
-			}
-
-			/**
-			 * 
-			 * @param numberOfClusters
-			 *            un string que representa un rango VÁLIDO (4..5) o un
-			 *            número
-			 * @return
-			 * @see PagesDescriptors#configurationPage()
-			 */
-			private Set<Integer> calculateRange(String numberOfClusters) {
-				// si es un número armo un rango de ese número hasta ese número,
-				// que a es equivalente
-				if (!numberOfClusters.contains(".."))
-					numberOfClusters = numberOfClusters + ".." + numberOfClusters;
-				String[] r = numberOfClusters.split("\\.\\.");
-				return Ranges.closed(new Integer(r[0]), new Integer(r[1])).asSet(DiscreteDomains.integers());
-			}
-
-			@Override
-			protected WizardModel createWizardModel() {
-				return new WizardModel()//
-						.add(PagesDescriptors.GENERATE_CLUSTER_CALCULATE_BIOLOGICAL_VALUE, new WritableValue(true, Boolean.class))//
-						// el "número" de clusters puede ser un rango
-						.add(PagesDescriptors.NUMBER_OF_CLUSTERS, new WritableValue("2..3", String.class))//
-						.add(PagesDescriptors.TIMES_TO_REPEAT_CLUSTERING, new WritableValue(10, Integer.class))//
-						.add(PagesDescriptors.VALIDATION_TYPE)//
-						.add(PagesDescriptors.ATTRIBUTE_TYPE, new WritableValue(null, AttributeTypeEnum.class))//
-						.add(PagesDescriptors.STATISTICAL_TEST_VALUE, new WritableValue("", String.class))//
-						.add(PagesDescriptors.ATTRIBUTE_NAME_TO_VALIDATION)//
-						.add(PagesDescriptors.SECOND_ATTRIBUTE_NAME_TO_VALIDATION)//
-						.add(PagesDescriptors.REMOVE_GENES_IN_BIOMARKER, new WritableValue(false, Boolean.class));
-			}
-
-			@Override
-			protected List<WizardPageDescriptor> createPagesDescriptors() {
-				return Arrays.asList(PagesDescriptors.experimentsWPD(), PagesDescriptors.configurationPage());
-			}
-
-			@Override
-			protected String getTaskName() {
-				return "Aplicando Experimentos";
+			protected void afterExecution() {
+				tr.refresh();
 			}
 		};
-		return w;
 	}
 }
