6cfbfb385686e5a8f4909170fbb75531b30b06b0
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-05-02 03:50:07 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@365 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/Activator.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/Activator.java
@@ -1,12 +1,13 @@
 package edu.unlp.medicine.bioplat.rcp.application;
 
+import javax.annotation.Nullable;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
-import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
-
 public class Activator extends AbstractUIPlugin {
 
+	private static final ImageDescriptor DEFAULT = imageDescriptorFromPlugin("resources/icons/default.png");
 	private static final String RESOURCES_ICONS = "resources/icons/";
 
 	public static String id() {
@@ -17,19 +18,17 @@ public class Activator extends AbstractUIPlugin {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * 
-	 * @deprecated Usar preferentemente
-	 *             {@link PlatformUIUtils#findImage(String)}
-	 */
-	@Deprecated
-	public static ImageDescriptor imageDescriptorFromPlugin(String imageFilePath) {
+	public static ImageDescriptor imageDescriptorFromPlugin(@Nullable String imageFilePath) {
+
+		if (imageFilePath == null)
+			return DEFAULT;
+
 		if (!imageFilePath.contains(RESOURCES_ICONS))
 			imageFilePath = RESOURCES_ICONS + imageFilePath;
 
 		ImageDescriptor id = imageDescriptorFromPlugin(id(), imageFilePath);
 		if (id == null)
-			id = imageDescriptorFromPlugin("resources/icons/default.png");
+			id = DEFAULT;
 		return id;
 
 	}
