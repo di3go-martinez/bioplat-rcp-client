85bf5013647dbe8fa8d86201b43c205bd650f250
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-05-17 02:30:58 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@399 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/actions/contributions/RemoveGenes.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/actions/contributions/RemoveGenes.java
@@ -2,24 +2,26 @@ package edu.unlp.medicine.bioplat.rcp.ui.biomarker.actions.contributions;
 
 import java.util.List;
 
-import com.google.common.collect.Lists;
-
-import edu.unlp.medicine.bioplat.rcp.ui.entities.editors.contributors.AbstractActionContribution;
-import edu.unlp.medicine.bioplat.rcp.ui.genes.dialogs.GenesInputDialog;
+import edu.unlp.medicine.bioplat.rcp.ui.experiment.actions.contributions.FreeGeneInputActionContribution;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
 import edu.unlp.medicine.entity.biomarker.Biomarker;
 import edu.unlp.medicine.entity.gene.Gene;
 
-public class RemoveGenes extends AbstractActionContribution<Biomarker> {
+public class RemoveGenes extends FreeGeneInputActionContribution<Biomarker> {
+
+	// @Override
+	// public void run() {
+	// GenesInputDialog gin = new GenesInputDialog();
+	// List<Gene> genes = Lists.newArrayList();
+	// if (gin.accepted())
+	// genes = gin.genes();
+	//
+	//
+	// }
 
 	@Override
-	public void run() {
-		GenesInputDialog gin = new GenesInputDialog();
-		List<Gene> genes = Lists.newArrayList();
-		if (gin.accepted())
-			genes = gin.genes();
-
+	protected void executeOn(List<Gene> genes) {
 		final MessageManager mmgr = MessageManager.INSTANCE;
 		for (Gene g : genes) {
 			if (!model().getGenes().contains(g))
