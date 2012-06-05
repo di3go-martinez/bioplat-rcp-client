6eb84af5c1b24d9501ff2f8c5e1d893da48a0d9c
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-06-05 03:06:24 +0000

    Wizard con páginas de resultado (prueba)
    git-svn-id: https://svn.riouxsvn.com/bioplat04@446 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/wizards/AbstractWizard.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/wizards/AbstractWizard.java
@@ -1,15 +1,16 @@
 package edu.unlp.medicine.bioplat.rcp.ui.entities.wizards;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.validation.ValidationStatus;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.databinding.wizard.WizardPageSupport;
 import org.eclipse.jface.dialogs.Dialog;
@@ -24,12 +25,17 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWizard;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.IProgressConstants;
+
+import com.google.common.collect.Maps;
 
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
+import edu.unlp.medicine.bioplat.rcp.utils.Holder;
 import edu.unlp.medicine.bioplat.rcp.utils.Monitors;
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 import edu.unlp.medicine.bioplat.rcp.utils.wizards.WizardModel;
+import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
 import edu.unlp.medicine.utils.monitor.Monitor;
 
 /**
@@ -46,7 +52,7 @@ import edu.unlp.medicine.utils.monitor.Monitor;
 public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWizard {
 
 	public AbstractWizard() {
-
+		setNeedsProgressMonitor(true);
 	}
 
 	private WizardModel model = createWizardModel();
@@ -64,17 +70,21 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 		return new WizardModel();
 	}
 
+	private Map<IWizardPage, WizardPageDescriptor> map = Maps.newHashMap();
+
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 
 		if (initialized)
 			return;
 		initialized = true;
+		IWizardPage page;
 		for (final WizardPageDescriptor pd : createPagesDescriptors()) {
-			addPage(new WizardPage(pd.getPageName(), pd.getTitle(), pd.getImageDescriptor()) {
+			addPage(page = new WizardPage(pd.getPageName(), pd.getTitle(), pd.getImageDescriptor()) {
 
 				@Override
 				public void createControl(Composite parent) {
+
 					final DataBindingContext dbc = new DataBindingContext();
 
 					WizardPageSupport.create(this, dbc);
@@ -83,6 +93,7 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 					fillDefaultsIfNecesary(control);
 
 					setControl(control);
+
 				}
 
 				private void fillDefaultsIfNecesary(Composite control) {
@@ -95,8 +106,47 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 					return pd.isPageComplete(/* this? */wizardModel());
 				}
 
+				@Override
+				public boolean canFlipToNextPage() {
+					return isPageComplete() && (pd.hasResultPage() || getNextPage() != null);
+				}
+
 			});
+
+			map.put(page, pd);
+
+			if (pd.hasResultPage()) {
+				// TODO pd.createResultPage();
+				IWizardPage p = new ResultPage(this, pd);
+				map.put(p, null);
+				addPage(p);
+			}
+
 		}
+	}
+
+	private boolean initialPage = true;
+
+	@Override
+	public IWizardPage getNextPage(IWizardPage page) {
+		// Caso especial...
+		if (initialPage) {
+			initialPage = false;
+			return super.getNextPage(page);
+		}
+
+		IWizardPage next = super.getNextPage(page);
+		if (next instanceof ResultPage) {
+			ResultPage p = (ResultPage) next;
+			p.lazyCreateControl();
+		}
+		return next;
+	}
+
+	@Override
+	public IWizardPage getPreviousPage(IWizardPage page) {
+		// TODO Auto-generated method stub
+		return super.getPreviousPage(page);
 	}
 
 	private boolean blockOnOpen = true;
@@ -148,7 +198,8 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 
 	protected abstract List<WizardPageDescriptor> createPagesDescriptors();
 
-	private static ExecutorService exec = Executors.newFixedThreadPool(1);
+	// TODO probar el pool, antes tenía 1 por problemas de concurrencia ¿?...
+	private static ExecutorService exec = Executors.newFixedThreadPool(10);
 
 	// TODO refactor
 	@Override
@@ -158,10 +209,14 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 
 		configureParameters();
 
+		// TODO migrar a EJob
 		Job j = new Job(getTaskName()) {
 
 			@Override
 			protected IStatus run(final IProgressMonitor progressMonitor) {
+
+				// TODO sacar cuando se migre a EJob
+				setProperty(IProgressConstants.KEEP_PROPERTY, true);
 
 				progressMonitor.beginTask(getTaskName(), IProgressMonitor.UNKNOWN);
 
@@ -174,12 +229,17 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 
 				});
 
+				// TODO revisar mejor lo del error holder y lo de status...
+				final Holder<Boolean> errorHolder = Holder.create(false);
+				IStatus status = ValidationStatus.ok();
 				T o = null;
 				try {
 					o = holder.get(); // join
 				} catch (Exception e) {
 					e.printStackTrace();
-					MessageManager.INSTANCE.add(Message.error("¡Error inesperado!", e));
+					MessageManager.INSTANCE.add(Message.error(errorMsg(), e));
+					errorHolder.hold(true);
+					status = ValidationStatus.error(errorMsg(), e);
 				}
 
 				final T oo = o;
@@ -188,17 +248,23 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 					@Override
 					public void run() {
 						try {
+							if (errorHolder.value())
+								return;
 							doInUI(oo);
-							MessageManager.INSTANCE.add(Message.info("¡Operación realizada con éxito!"));
+							MessageManager.INSTANCE.add(Message.info(getTaskName() + " was succesfully executed"));
 						} catch (Exception e) {
 							e.printStackTrace();
-							MessageManager.INSTANCE.add(Message.error("¡Error inesperado!", e));
+							MessageManager.INSTANCE.add(Message.error(errorMsg(), e));
 						}
 					}
 				});
 
 				progressMonitor.done();
-				return Status.OK_STATUS;
+				return status;
+			}
+
+			private String errorMsg() {
+				return "Unexpected error executing " + getTaskName();
 			}
 
 		};
@@ -247,4 +313,40 @@ public abstract class AbstractWizard<T> extends Wizard implements IWorkbenchWiza
 	 */
 	protected abstract void doInUI(T result) throws Exception;
 
+}
+
+class ResultPage extends WizardPage {
+
+	private Composite parent;
+	private boolean initialized = false;
+	private WizardPageDescriptor pd; // PageDescriptor
+	private AbstractWizard<?> w;
+
+	public ResultPage(AbstractWizard<?> w, WizardPageDescriptor pd) {
+		super("dummy page");
+		this.pd = pd;
+		this.w = w;
+	}
+
+	@Override
+	public void createControl(Composite rparent) {
+		this.parent = Widgets.createDefaultContainer(rparent);
+		setControl(this.parent);
+	}
+
+	void lazyCreateControl() {
+		if (!initialized) {
+			pd.initializeResultPage(parent, w.wizardModel(), w, this);
+			parent.layout(true);
+			initialized = true;
+		} else {
+			pd.refreshResultPage(w.wizardModel(), w);
+		}
+
+	}
+
+	@Override
+	public boolean isPageComplete() {
+		return pd.isResultPageComplete(w.wizardModel());
+	}
 }
--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/wizards/WizardPageDescriptor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/entities/wizards/WizardPageDescriptor.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Composite;
 
@@ -69,6 +70,44 @@ public abstract class WizardPageDescriptor {
 	}
 
 	public boolean isPageComplete(WizardModel model) {
+		return true;
+	}
+
+	/**
+	 * Indica si está página origina otra página que sería de resultado, y por
+	 * ende, calculada dinámicamente
+	 * 
+	 * @return
+	 */
+	// TODO soporte para que pueda cambiar "dinámica", por ahora si se crea en
+	// false, luego no podrá cambiar a true, tampoco al reves. Como workaround
+	// se propone crearla en true y en la correspondiente página de resultado
+	// poner que no se requiere configuración y que siga a la próxima
+	public boolean hasResultPage() {
+		return false;
+	}
+
+	/**
+	 * 
+	 * Inicializa una página de resultado
+	 * 
+	 * @param parent
+	 * @param wizardModel
+	 */
+	public void initializeResultPage(Composite parent, WizardModel wizardModel, IWizard wizard, WizardPage resultPage) {
+		createResultPage(parent, wizardModel, wizard, resultPage);
+		refreshResultPage(wizardModel, wizard);
+	}
+
+	protected void createResultPage(Composite parent, WizardModel wizardModel, IWizard wizard, WizardPage resultPage) {
+		// nada, por default no hay página de resultados a crear
+	}
+
+	public void refreshResultPage(WizardModel wizardModel, IWizard wizard) {
+		// nada, por default no hay página de resultados a refrescar
+	}
+
+	public boolean isResultPageComplete(WizardModel wizardModel) {
 		return true;
 	}
 
