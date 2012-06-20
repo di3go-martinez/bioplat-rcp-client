2cff8487c51aab185b0e3f676a8a0d9b4813d102
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-06-20 16:24:41 +0000

    Agrego listener para el shutdown de la vm y del servidor R Agrego informe de error si no se pudo levantar el servidor de R en la vista de mensajes
    git-svn-id: https://svn.riouxsvn.com/bioplat04@489 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/startup/StartupRserve.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/startup/StartupRserve.java
@@ -1,7 +1,5 @@
 package edu.unlp.medicine.bioplat.rcp.application.startup;
 
-import java.io.IOException;
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -10,6 +8,8 @@ import org.eclipse.ui.IStartup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
+import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
 import edu.unlp.medicine.r4j.constants.OSDependentConstants;
 import edu.unlp.medicine.r4j.environments.RServeConfigurator;
 
@@ -17,32 +17,48 @@ public class StartupRserve implements IStartup {
 	// Logger Object
 	private static Logger logger = LoggerFactory.getLogger(StartupRserve.class);
 
+	private static final String R_PATH = OSDependentConstants.PATH_TO_R;
+	private Process process;
+
 	@Override
 	public void earlyStartup() {
+
 		new Job("Rserve - Initializing...") {
 
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
 				monitor.beginTask("Initializing rserve", IProgressMonitor.UNKNOWN);
 
-				Process process;
-				String R_PATH = OSDependentConstants.PATH_TO_R;
 				int port = RServeConfigurator.getInstance().getPort();
 				try {
 					process = Runtime.getRuntime().exec(R_PATH + " --save --restore -q -e library('Rserve');Rserve(port=" + port + ")");
 					process.getInputStream();
 					// sleep
 					Thread.sleep(800);
-				} catch (IOException e) {
-					logger.error("Failed to run the Rserve on port " + port + " on the path:" + R_PATH);
-					e.printStackTrace();
 				} catch (Exception e) {
-					logger.error("Failed to run the Rserve on port " + port + " on the path:" + R_PATH);
+					final String msg = "Failed to run the Rserve on port " + port + " on the path:" + R_PATH;
+					logger.error(msg, e);
 					e.printStackTrace();
+					MessageManager.INSTANCE.add(Message.error(msg, e));
 				}
 
 				return Status.OK_STATUS;
 			}
 		}.schedule();
+
+		// Agrego un listener para cuado se baje la aplicación se ejecute
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+
+			@Override
+			public void run() {
+				// FIXME implementar el shutdown del Rserve o un kill
+				// if (process != null) {
+				// process.destroy();
+				// process = null;
+				// }
+
+			}
+
+		});
 	}
 }
