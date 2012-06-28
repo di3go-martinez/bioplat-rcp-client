7f288191d4bd349b6b83cb7cc8214b73aaa93a20
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-06-28 15:59:09 +0000

    Menú contextual para abrir los elementos seleccionados o enfocados
    git-svn-id: https://svn.riouxsvn.com/bioplat04@525 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/wizards/pso/PSOResultViewPart.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/wizards/pso/PSOResultViewPart.java
@@ -3,12 +3,20 @@ package edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.part.ViewPart;
 
+import edu.unlp.medicine.bioplat.rcp.ui.entities.EditorsId;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.ColumnBuilder;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableBuilder;
+import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableBuilder.MenuBuilder;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableReference;
+import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.menues.MenuItemContribution;
+import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
 import edu.unlp.medicine.domainLogic.framework.optimizers.BiomarkerOptimizationResult;
 import edu.unlp.medicine.entity.biomarker.Biomarker;
@@ -35,12 +43,34 @@ public class PSOResultViewPart extends ViewPart {
 		else
 			list = Collections.emptyList();
 
-		tref = TableBuilder.create(container).hideSelectionColumn().input(list) //
+		tref = TableBuilder.create(container).input(list) //
 				.addColumn(ColumnBuilder.create().property("name").title("Name"))//
 				.addColumn(ColumnBuilder.create().numeric().property("significanceValue.pvalue").title("p-value"))//
 				.addColumn(ColumnBuilder.create().numeric().property("numberOfGenes").title("Genes"))//
-				.build();
+				.contextualMenuBuilder(createMenuBuilder()).build();
 
+	}
+
+	private MenuBuilder createMenuBuilder() {
+		return new MenuBuilder() {
+
+			@Override
+			public void build(Menu menu) {
+				Image openImage = PlatformUIUtils.findImage("openSelection.gif");
+				MenuItemContribution.create(menu).image(openImage).text("Open Selection").addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						// intenta abrir los seleccionados o si no hay
+						// seleccionados, abre el que tenga el foco
+						List<Biomarker> biomarkers = tref.selectedElements();
+						if (biomarkers.isEmpty())
+							biomarkers = tref.focusedElements();
+						for (Biomarker b : biomarkers)
+							PlatformUIUtils.openEditor(b, EditorsId.biomarkerEditorId());
+					}
+				});
+			}
+		};
 	}
 
 	@Override
