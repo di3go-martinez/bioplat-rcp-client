197263f77c4f477748f0995418baacac6eb325e4
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-06-28 12:57:21 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@516 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/editor/EditorDescription.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/editor/EditorDescription.java
@@ -1,8 +1,18 @@
 package edu.unlp.medicine.bioplat.rcp.editor;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
 
+/**
+ * 
+ * @author Diego Martínez
+ * 
+ */
 public class EditorDescription {
 
 	private IEditorInput input;
@@ -30,4 +40,58 @@ public class EditorDescription {
 	public String title() {
 		return title;
 	}
+
+	public static EditorDescription errorEditorDescription(String msg, Throwable exception) {
+		return new EditorDescription(null, new ErrorPart(), "Error");
+	}
+
+	public static EditorDescription errorEditorDescription(String msg) {
+		return errorEditorDescription(msg, null);
+	}
+}
+
+class ErrorPart extends EditorPart {
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDirty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
