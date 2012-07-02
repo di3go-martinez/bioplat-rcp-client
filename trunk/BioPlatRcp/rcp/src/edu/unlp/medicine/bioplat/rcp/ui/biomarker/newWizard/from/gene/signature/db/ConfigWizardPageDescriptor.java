6f14e4c6764799725563ea116bea33aa8925fe26
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-07-02 04:27:01 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@571 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/newWizard/from/gene/signature/db/ConfigWizardPageDescriptor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/newWizard/from/gene/signature/db/ConfigWizardPageDescriptor.java
@@ -6,6 +6,7 @@ import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.jface.databinding.viewers.ViewersObservables;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -30,7 +31,9 @@ public class ConfigWizardPageDescriptor extends WizardPageDescriptor {
 
 	public ConfigWizardPageDescriptor(WizardModel wmodel) {
 		super("Configuration");
+
 		addParameters(wmodel);
+
 	}
 
 	static final String DATABASE = "DATABASE";
@@ -38,6 +41,10 @@ public class ConfigWizardPageDescriptor extends WizardPageDescriptor {
 
 	@Override
 	public Composite create(WizardPage wizardPage, Composite parent, DataBindingContext dbc, WizardModel wmodel) {
+		wizardPage.setTitle("Copy gene signature from external database");
+		wizardPage.setDescription("It creates your own copy of a Bioplat geneSignature containing all the information of the original gene Signature");
+
+		GridDataFactory gdf = GridDataFactory.fillDefaults().grab(true, false);
 
 		Composite container = new Composite(parent, SWT.BORDER);
 		container.setLayout(GridLayoutFactory.fillDefaults().margins(10, 10).numColumns(2).create());
@@ -45,15 +52,18 @@ public class ConfigWizardPageDescriptor extends WizardPageDescriptor {
 		ComboViewer cv = Utils.newComboViewer(container, "Database:", Arrays.asList(Constants.GENE_SIG_DB, Constants.MOL_SIG_DB));
 		dbc.bindValue(ViewersObservables.observeSingleSelection(cv), wmodel.valueHolder(DATABASE));
 		cv.setSelection(new StructuredSelection(Constants.GENE_SIG_DB));
+		cv.getCombo().setLayoutData(gdf.create());
 
 		new Label(container, SWT.NONE).setText("Gene Signature Name or Id");
 		Text t = new Text(container, SWT.BORDER);
 		dbc.bindValue(SWTObservables.observeText(t, SWT.Modify), wmodel.valueHolder(GENE_SIGNATURE_OR_ID), UpdateStrategies.nonNull(GENE_SIGNATURE_OR_ID), null);
+		t.setLayoutData(gdf.create());
 
 		return container;
 	}
 
 	ConfigWizardPageDescriptor addParameters(WizardModel wmodel) {
+
 		wmodel.add(DATABASE);
 		wmodel.add(GENE_SIGNATURE_OR_ID, new WritableValue("", String.class));
 		return this;
