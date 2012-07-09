84039858bf67524557301d6d50f544ecb5f2129c
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2012-07-09 00:02:14 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@622 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/BioPlatPerspective.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/BioPlatPerspective.java
@@ -11,7 +11,6 @@ public class BioPlatPerspective implements IPerspectiveFactory {
 	public void createInitialLayout(IPageLayout layout) {
 		layout.addPlaceholder("*view*", IPageLayout.BOTTOM, 0.6f, layout.getEditorArea());
 
-		// TODO habilitar cuando funcione bien la vista de genes
 		layout.addView(GeneViewPart.id(), IPageLayout.RIGHT, 0.5f, layout.getEditorArea());
 
 		// pruebas... sacar de acá...
