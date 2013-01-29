09f29ba264f6e997f3f109cf52c0a39e176297f5
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-01-29 12:18:57 +0000

    Fix: No cargaba correctamente las urls configuradas en la vista de genes
    git-svn-id: https://svn.riouxsvn.com/bioplat04@677 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/edu.unlp.medicine.bioplat.core/src/main/java/edu/unlp/medicine/bioplat/rcp/utils/PlatformUtils.java
+++ trunk/BioPlatRcp/edu.unlp.medicine.bioplat.core/src/main/java/edu/unlp/medicine/bioplat/rcp/utils/PlatformUtils.java
@@ -3,15 +3,14 @@ package edu.unlp.medicine.bioplat.rcp.utils;
 import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
-import edu.unlp.medicine.bioplat.core.Activator;
-
 public class PlatformUtils {
 
 	private PlatformUtils() {
 	}
 
 	public static IEclipsePreferences preferences() {
-		return ConfigurationScope.INSTANCE.getNode(Activator.id());
+		// TODO externalizar el id del activator del plugin de genes!!
+		return ConfigurationScope.INSTANCE.getNode("edu.unlp.medicine.bioplat.genes");
 	}
 
 }
