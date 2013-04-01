008fd18353ee4c16fd9b9a9d4906d9451125cf01
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-04-01 12:52:07 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@767 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/edu.unlp.medicine.bioplat.core/src/main/java/edu/unlp/medicine/bioplat/rcp/editor/Constants.java
+++ trunk/BioPlatRcp/edu.unlp.medicine.bioplat.core/src/main/java/edu/unlp/medicine/bioplat/rcp/editor/Constants.java
@@ -5,6 +5,7 @@ public interface Constants {
 	String DEFAULT = "default key";
 	String GENE_SIGNATURE = "gene signature key";
 	Object MODEL = "model";
+	// TODO RENOMBRAR si siempre guarda el único gen seleccionado
 	String GENES = "genes key";
 	String SELECTED_GENES = "selected genes key";
 
