1e71d7dc25a935e35b70c847b5512a0712306971
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-04-04 22:12:19 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@794 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/resources/R/librariesToBioplat.r
+++ trunk/BioPlatRcp/rcp/resources/R/librariesToBioplat.r
@@ -1,3 +1,4 @@
 inSilicoDb=source("http://www.bioconductor.org/biocLite.R")
 limma=source("http://bioconductor.org/biocLite.R");biocLite("limma");biocLite(c("GenomicFeatures", "AnnotationDbi"));biocLite()
 frma=source("http://bioconductor.org/biocLite.R");biocLite("frma")
+survcomp=source("http://bioconductor.org/biocLite.R");biocLite("survcomp")
