af77cc8c0c284738cf55a52c00f7b9acccb80009
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-07-19 22:52:09 +0000

    con posibilidad de seteo de uso del cluster precalculado en el experimento
    git-svn-id: https://svn.riouxsvn.com/bioplat04@853 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/wizards/optimization/ValidationConfigPageDescriptor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/wizards/optimization/ValidationConfigPageDescriptor.java
@@ -2,8 +2,6 @@ package edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.optimization;
 
 import java.util.ArrayList;
 
-import edu.unlp.medicine.bioplat.rcp.config.StatisticConfigGUI;
-import edu.unlp.medicine.bioplat.rcp.ui.experiment.actions.contributions.*;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.wizard.WizardPage;
@@ -18,6 +16,7 @@ import org.eclipse.swt.widgets.Group;
 
 import com.google.common.collect.ImmutableList;
 
+import edu.unlp.medicine.bioplat.rcp.config.StatisticConfigGUI;
 import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.WizardPageDescriptor;
 import edu.unlp.medicine.bioplat.rcp.ui.experiment.actions.contributions.ValidationConfigWizard;
 import edu.unlp.medicine.bioplat.rcp.utils.wizards.WizardModel;
@@ -25,10 +24,8 @@ import edu.unlp.medicine.bioplat.rcp.widgets.Widget;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
 import edu.unlp.medicine.bioplat.rcp.widgets.listeners.ModificationListener;
 import edu.unlp.medicine.bioplat.rcp.widgets.listeners.ModificationTextEvent;
-import edu.unlp.medicine.config.StatisticConfig;
 import edu.unlp.medicine.domainLogic.ext.metasignatureCommands.LogRankTestCommand;
 import edu.unlp.medicine.domainLogic.framework.metasignatureCommands.OneBiomarkerCommand;
-import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.LogRankTestValidationConfig;
 import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.ValidationConfig4DoingCluster;
 import edu.unlp.medicine.entity.biomarker.Biomarker;
 
@@ -42,6 +39,7 @@ public class ValidationConfigPageDescriptor extends WizardPageDescriptor {
 	Image image4Button;
 	String type;
 	String description;
+
 	/**
 	 * 
 	 * @param biomarker
@@ -51,14 +49,15 @@ public class ValidationConfigPageDescriptor extends WizardPageDescriptor {
 	 *            wizardmodel
 	 */
 	public ValidationConfigPageDescriptor(Biomarker biomarker, String type, String description, String wmodelKey, Image image4Button) {
-		super("Select the experiment to do "+ type);
+		super("Select the experiment to do " + type);
 		this.biomarker = biomarker;
 		this.wizardModelKey = wmodelKey;
-		this.image4Button=image4Button;
+		this.image4Button = image4Button;
 		this.type = type;
 		this.description = description;
 	}
 
+	private Widget useExistingExperimentGroup;
 	private Composite innerContainer;
 	// innercomposite's content
 	private Widget experimentName;
@@ -66,29 +65,29 @@ public class ValidationConfigPageDescriptor extends WizardPageDescriptor {
 	private Widget attribtueNameToDoTheValidation;
 	private Widget numberOfTimesToRepeatTheCluster;
 	private Widget attribtueNameToDoTheValidation2;
-	
-	
+
 	private boolean optional;
 
 	@Override
 	public Composite create(final WizardPage wizardPage, Composite parent, DataBindingContext dbc, final WizardModel wmodel) {
 
-		//Composite container = Widgets.createDefaultContainer(parent);
+		// Composite container = Widgets.createDefaultContainer(parent);
 		wizardPage.setDescription(description);
 
 		GridData extensor = new GridData();
-		extensor.horizontalAlignment=SWT.CENTER;
-		extensor.grabExcessHorizontalSpace=true;
-		extensor.verticalIndent=300;
-		
-		Composite group = new Group(parent, SWT.NONE);
-		group.setLayout(GridLayoutFactory.fillDefaults().numColumns(1).margins(50,50).spacing(2, 50).create());
+		extensor.horizontalAlignment = SWT.CENTER;
+		extensor.grabExcessHorizontalSpace = true;
+		extensor.verticalIndent = 300;
+
+		final Composite group = new Group(parent, SWT.NONE);
+		group.setLayout(GridLayoutFactory.fillDefaults().numColumns(1).margins(50, 50).spacing(2, 50).create());
 		group.setLayoutData(extensor);
 
 		Button b = new Button(group, SWT.PUSH);
 		b.setText("Select " + type + " dataset (Bioplat Experiment in your desktop)");
 		b.setImage(image4Button);
-		GridData gd = new GridData(400,70);gd.horizontalAlignment = SWT.CENTER;
+		GridData gd = new GridData(400, 70);
+		gd.horizontalAlignment = SWT.CENTER;
 		b.setLayoutData(gd);
 		b.addSelectionListener(new SelectionAdapter() {
 
@@ -103,9 +102,7 @@ public class ValidationConfigPageDescriptor extends WizardPageDescriptor {
 					}
 
 					@Override
-					public OneBiomarkerCommand createCommand(
-							Biomarker aBiomarker,
-							ArrayList<ValidationConfig4DoingCluster> validationConfigs) {
+					public OneBiomarkerCommand createCommand(Biomarker aBiomarker, ArrayList<ValidationConfig4DoingCluster> validationConfigs) {
 						// TODO Auto-generated method stub
 						return new LogRankTestCommand(aBiomarker, validationConfigs);
 					}
@@ -114,23 +111,35 @@ public class ValidationConfigPageDescriptor extends WizardPageDescriptor {
 				vcw.open();
 			}
 
-			private void update(ValidationConfig4DoingCluster config) {
+			private void update(final ValidationConfig4DoingCluster config) {
 
 				if (!contentsCreated) {
+					useExistingExperimentGroup = Widgets.createCheckBox(group, "Use cluster configured in the experiment", config, "useExperimentGroups").readOnly().addModificationListener(new ModificationListener() {
+
+						@Override
+						public void modify(ModificationTextEvent event) {
+							innerContainer.setVisible(!config.isUseExperimentGroups());
+						}
+					});
+
 					experimentName = Widgets.createTextWithLabel(innerContainer, "Experiment Name", config, "experimentToValidate.name").readOnly();
 					numberOfClusters = Widgets.createTextWithLabel(innerContainer, "Clusters", config, "numberOfClusters").readOnly();
 					numberOfTimesToRepeatTheCluster = Widgets.createTextWithLabel(innerContainer, "Times", config, "numberOfTimesToRepeatTheCluster").readOnly();
 					attribtueNameToDoTheValidation = Widgets.createTextWithLabel(innerContainer, "Validation Attribute 1", config, "attribtueNameToDoTheValidation").readOnly();
 					attribtueNameToDoTheValidation2 = Widgets.createTextWithLabel(innerContainer, "Validation Attribute 2", config, "secondAttribtueNameToDoTheValidation").readOnly();
-					//statisticsSignificanceTest = Widgets.createTextWithLabel(innerContainer, "Statistics Significance Test", config, "statisticsSignificanceTest.friendlyName").readOnly();
+					// statisticsSignificanceTest =
+					// Widgets.createTextWithLabel(innerContainer,
+					// "Statistics Significance Test", config,
+					// "statisticsSignificanceTest.friendlyName").readOnly();
 
-					
+					group.layout();
 					innerContainer.layout();
 					contentsCreated = true;
 				}
 
-				Widgets.retarget(config, experimentName, numberOfClusters, numberOfTimesToRepeatTheCluster, attribtueNameToDoTheValidation, attribtueNameToDoTheValidation2);
-
+				Widgets.retarget(config, experimentName, numberOfClusters, numberOfTimesToRepeatTheCluster, attribtueNameToDoTheValidation, attribtueNameToDoTheValidation2, useExistingExperimentGroup);
+				// TODO hacer que en función del useExistingExperimentGroup se
+				// muestre o no y/o como los datos de la configuración!!!
 				wmodel.set(wizardModelKey, ImmutableList.of(config));
 				// TODO revisar mejor: actualizo los botones del wizard
 				// explícitamente, porque cambio el estado con la asignación del
@@ -161,6 +170,5 @@ public class ValidationConfigPageDescriptor extends WizardPageDescriptor {
 		disableClusterRanges = true;
 		return this;
 	}
-	
-		
+
 }
