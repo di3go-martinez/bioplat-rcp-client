77365975d90d094b021f3d9c06ef9eeac53e6623
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-07-19 22:54:26 +0000

    formatter
    git-svn-id: https://svn.riouxsvn.com/bioplat04@857 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/wizards/pso/PSOWizard.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/wizards/pso/PSOWizard.java
@@ -1,7 +1,6 @@
 package edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso;
 
 import static edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso.page.descriptors.GeneralPSOConfigurarion.MINIMUM_NUMBER_OF_GENES;
-//import static edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso.page.descriptors.GeneralPSOConfigurarion.NUMBER_OF_GENE_SIGNATURE_TO_KEEP_DURING_TRAINING;
 import static edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso.page.descriptors.GeneralPSOConfigurarion.NUMBER_OF_PARTICLES;
 import static edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso.page.descriptors.GeneralPSOConfigurarion.NUMBER_OF_ROUNDS;
 import static edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso.page.descriptors.GeneralPSOConfigurarion.PROCESS_NAME;
@@ -16,13 +15,11 @@ import com.google.common.collect.Lists;
 
 import edu.unlp.medicine.bioplat.core.Activator;
 import edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.optimization.ValidationConfigPageDescriptor;
-import edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.optimization.blindSearch.BlindSearchResultViewPart;
 import edu.unlp.medicine.bioplat.rcp.ui.biomarker.wizards.pso.page.descriptors.GeneralPSOConfigurarion;
 import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.AbstractWizard;
 import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.WizardPageDescriptor;
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 import edu.unlp.medicine.bioplat.rcp.utils.wizards.WizardModel;
-import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.LogRankTestValidationConfig;
 import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.ValidationConfig4DoingCluster;
 import edu.unlp.medicine.domainLogic.framework.optimizers.BiomarkerOptimizationResult;
 import edu.unlp.medicine.domainLogic.framework.optimizers.IObjectWithFitness;
@@ -34,7 +31,7 @@ import edu.unlp.medicine.utils.monitor.Monitor;
 /**
  * @author Diego Martínez
  */
-public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> implements Observer{
+public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> implements Observer {
 
 	public static final String TESTING_VALIDATION_CONFIG = "TESTING_VALIDATION_CONFIG";
 	public static final String VALIDATION_VALIDATION_CONFIG = "VALIDATION_VALIDATION_CONFIG";
@@ -43,12 +40,12 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 	private String processName;
 	private int miniumNumberOfGenes, numberOfRounds, numberOfParticles, numberOfGenesToKeepDuringTraining;
 
-	private List<ValidationConfig4DoingCluster> forTesting=new ArrayList<ValidationConfig4DoingCluster>();
-	private List<ValidationConfig4DoingCluster> forTraining=new ArrayList<ValidationConfig4DoingCluster>();
-	private List<ValidationConfig4DoingCluster> forValidation=new ArrayList<ValidationConfig4DoingCluster>();
-	
+	private List<ValidationConfig4DoingCluster> forTesting = new ArrayList<ValidationConfig4DoingCluster>();
+	private List<ValidationConfig4DoingCluster> forTraining = new ArrayList<ValidationConfig4DoingCluster>();
+	private List<ValidationConfig4DoingCluster> forValidation = new ArrayList<ValidationConfig4DoingCluster>();
+
 	private PSOResultViewPart resultView;
-	PsoOptimizerCommand optimizerCommand;	
+	PsoOptimizerCommand optimizerCommand;
 
 	public PSOWizard(Biomarker biomarker) {
 		this.biomarker = biomarker;
@@ -62,11 +59,13 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 		pages.add(new ValidationConfigPageDescriptor(biomarker, "Testing", "If you set this dataset, it will be used for ordering the result. YOu should take the first one as the result of the algorithm.", TESTING_VALIDATION_CONFIG, Activator.imageDescriptorFromPlugin("experiment-testing.png").createImage()).optional().setImageDescriptor(Activator.imageDescriptorFromPlugin("pso.png")));
 		pages.add(new ValidationConfigPageDescriptor(biomarker, "Validation", "This is the validation dataset. This woul be used for presenting results because it was independent of training and testing.", VALIDATION_VALIDATION_CONFIG, Activator.imageDescriptorFromPlugin("experiment-validation.png").createImage()).optional().setImageDescriptor(Activator.imageDescriptorFromPlugin("pso.png")));
 
-//		pages.add(new ValidationConfigPageDescriptor(biomarker, "Training", TRAINING_VALIDATION_CONFIG,null).disableClusterRange());
-//		pages.add(new ValidationConfigPageDescriptor(biomarker, "Testing", TESTING_VALIDATION_CONFIG,null).optional());
-//		pages.add(new ValidationConfigPageDescriptor(biomarker, "Validation", VALIDATION_VALIDATION_CONFIG,null).optional());
+		// pages.add(new ValidationConfigPageDescriptor(biomarker, "Training",
+		// TRAINING_VALIDATION_CONFIG,null).disableClusterRange());
+		// pages.add(new ValidationConfigPageDescriptor(biomarker, "Testing",
+		// TESTING_VALIDATION_CONFIG,null).optional());
+		// pages.add(new ValidationConfigPageDescriptor(biomarker, "Validation",
+		// VALIDATION_VALIDATION_CONFIG,null).optional());
 
-		
 		// pages.add(new ValidationToDoPageDescriptor());
 		return pages;
 	}
@@ -80,11 +79,13 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 	protected void doInUI(BiomarkerOptimizationResult result) throws Exception {
 		PlatformUIUtils.openView(PSOResultViewPart.id());
 		PSOResultViewPart v = (PSOResultViewPart) PlatformUIUtils.findView(PSOResultViewPart.id());
-		
+
 		v.setForTraining(forTraining.get(0));
-		if (forTesting!=null && forTesting.size()>0) v.setForTesting(forTesting.get(0));
-		if (forValidation!=null && forValidation.size()>0) v.setForValidation(forValidation.get(0));
-		
+		if (forTesting != null && forTesting.size() > 0)
+			v.setForTesting(forTesting.get(0));
+		if (forValidation != null && forValidation.size() > 0)
+			v.setForValidation(forValidation.get(0));
+
 		v.setResultToShow(result.getBettersTouchedDuringTheTrip());
 	}
 
@@ -96,26 +97,25 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 		psoConfig.setNumberOfGenes(miniumNumberOfGenes);
 		psoConfig.setNumberOfRounds(numberOfRounds);
 		psoConfig.setNumberOfParticles(numberOfParticles);
-		//optimizerCommand.setBettersSize(numberOfGenesToKeepDuringTraining);
+		// optimizerCommand.setBettersSize(numberOfGenesToKeepDuringTraining);
 		optimizerCommand.setConfiguration(psoConfig);
 
 		optimizerCommand.setValidationConfig4CLustering4Training(forTraining.get(0));
-		if (forValidation != null) optimizerCommand.setValidationConfig4Clustering4Validation(forValidation.get(0));
-		if (forTesting != null) optimizerCommand.setValidationConfig4Clustering4Testing(forTesting.get(0));
+		if (forValidation != null)
+			optimizerCommand.setValidationConfig4Clustering4Validation(forValidation.get(0));
+		if (forTesting != null)
+			optimizerCommand.setValidationConfig4Clustering4Testing(forTesting.get(0));
 
-		
 		resultView = PlatformUIUtils.openView(PSOResultViewPart.id());
 		resultView.setForTraining(forTraining.get(0));
 		if (forTesting != null && forTesting.size() > 0)
 			resultView.setForTesting(forTesting.get(0));
 		if (forValidation != null && forValidation.size() > 0)
 			resultView.setForValidation(forValidation.get(0));
-		
-		
-		
+
 		// va escuchando y completando con resultados a medida que los tiene
 		optimizerCommand.addObserver(this);
-		
+
 		optimizerCommand.monitor(monitor).execute();
 		return optimizerCommand.getPsoResult();
 	}
@@ -127,7 +127,9 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 				.add(MINIMUM_NUMBER_OF_GENES, Integer.class, 3)//
 				.add(NUMBER_OF_ROUNDS, Integer.class, 100)//
 				.add(NUMBER_OF_PARTICLES, Integer.class, 5)//
-				//.add(NUMBER_OF_GENE_SIGNATURE_TO_KEEP_DURING_TRAINING, Integer.class, 10)//
+		//
+		// .add(NUMBER_OF_GENE_SIGNATURE_TO_KEEP_DURING_TRAINING, Integer.class,
+		// 10)//
 		// No se agregan porque no son IObservables...
 		// FIXME mejorar...
 		// .add(TRAINING_VALIDATION_CONFIG, List.class, null). //
@@ -145,7 +147,8 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 		miniumNumberOfGenes = wm.value(MINIMUM_NUMBER_OF_GENES);
 		numberOfRounds = wm.value(NUMBER_OF_ROUNDS);
 		numberOfParticles = wm.value(NUMBER_OF_PARTICLES);
-		//numberOfGenesToKeepDuringTraining = wm.value(NUMBER_OF_GENE_SIGNATURE_TO_KEEP_DURING_TRAINING);
+		// numberOfGenesToKeepDuringTraining =
+		// wm.value(NUMBER_OF_GENE_SIGNATURE_TO_KEEP_DURING_TRAINING);
 
 		forTraining = wm.value(TRAINING_VALIDATION_CONFIG);
 		forValidation = wm.value(VALIDATION_VALIDATION_CONFIG);
@@ -157,10 +160,10 @@ public class PSOWizard extends AbstractWizard<BiomarkerOptimizationResult> imple
 	public void update(Observable o, Object arg) {
 		List<Biomarker> biomarkers = new ArrayList<Biomarker>();
 		for (IObjectWithFitness biomarker : optimizerCommand.getActualBettersDuringTheTrip()) {
-			biomarkers.add((Biomarker)biomarker);
+			biomarkers.add((Biomarker) biomarker);
 		}
-		
+
 		resultView.updateResults(biomarkers);
-		
+
 	}
 }
