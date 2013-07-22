6c85d31542fc5c1eede4277bbc856f7ede0d360f
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-07-22 03:38:33 +0000

    multi configuracion de cluster id fix: no se podía editar un cluster ya configurado en un experimento
    git-svn-id: https://svn.riouxsvn.com/bioplat04@859 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/medicine/bioplat/rcp/ConfigureClusterDialog.java
+++ trunk/BioPlatRcp/rcp/src/edu/medicine/bioplat/rcp/ConfigureClusterDialog.java
@@ -4,21 +4,28 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.primitives.Ints;
 
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.ColumnBuilder;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableBuilder;
+import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableBuilder.MenuBuilder;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableReference;
+import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.menues.MenuItemContribution;
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
 import edu.unlp.medicine.entity.experiment.AbstractExperiment;
@@ -28,6 +35,7 @@ public class ConfigureClusterDialog extends Dialog {
 
 	private AbstractExperiment experiment;
 	private List<Data> data;
+	private TableReference tref;
 
 	protected ConfigureClusterDialog(AbstractExperiment experiment) {
 		super(PlatformUIUtils.findShell());
@@ -37,9 +45,9 @@ public class ConfigureClusterDialog extends Dialog {
 	@Override
 	protected Control createContents(Composite parent) {
 		Composite container = Widgets.createDefaultContainer(parent);
-		TableReference ref = TableBuilder.create(container).input(data = createData())//
+		tref = TableBuilder.create(container).input(data = createData())//
 				.addColumn(ColumnBuilder.create().property("sample"))//
-				.addColumn(ColumnBuilder.create().editable().property("groupid")).build();
+				.addColumn(ColumnBuilder.create().editable().property("groupid")).contextualMenuBuilder(menuBuilder()).build();
 		Button ok = new Button(container, SWT.NONE);
 		ok.setText("OK");
 		ok.setLayoutData(GridDataFactory.fillDefaults().align(SWT.CENTER, SWT.CENTER).create());
@@ -53,10 +61,57 @@ public class ConfigureClusterDialog extends Dialog {
 		return container;
 	}
 
+	private MenuBuilder menuBuilder() {
+		
+		return new MenuBuilder() {
+
+			@Override
+			public void build(Menu menu) {
+				Image openImage = PlatformUIUtils.findImage("setcluster.gif");
+				MenuItemContribution.create(menu).image(openImage).text("Set cluster...").addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+
+						// abre un diálogo para ingresar un valor, lo setea a
+						// cada data de los seleccionados
+
+						InputDialog id = new InputDialog(PlatformUIUtils.findShell(), "Set cluster id", "Condigure a number id to cluster", "", new IInputValidator() {
+
+							@Override
+							public String isValid(String newText) {
+								if (Ints.tryParse(newText) == null)
+									return "The input '" + newText + "' is not a valid cluster id";
+								return null;
+							}
+						});
+						if (id.open() != Dialog.OK)
+							return;
+
+						Integer clusterid = Ints.tryParse(id.getValue());
+						List<Data> sampleData = tref.selectedElements();
+						if (sampleData.isEmpty())
+							sampleData = tref.focusedElements();
+						for (Data datum : sampleData)
+							datum.setGroupid(clusterid);
+
+						tref.refresh();
+					}
+				});
+			}
+		};
+	}
+
 	private List<Data> createData() {
 		List<Data> result = Lists.newArrayList();
-		for (Sample s : experiment.getSamples())
-			result.add(new Data(s, "-1"));
+
+		Map<Sample, Integer> groups = experiment.getGroups();
+
+		if (groups != null && !groups.isEmpty())
+			for (Map.Entry<Sample, Integer> entry : groups.entrySet())
+				result.add(new Data(entry.getKey(), entry.getValue().toString()));
+		else
+			for (Sample s : experiment.getSamples())
+				result.add(new Data(s, "-1"));
 		return result;
 
 	}
@@ -79,12 +134,18 @@ public class ConfigureClusterDialog extends Dialog {
 			this.groupid = i;
 		}
 
+		// FIXME hacer que groupid sea un integer y no un string?
+		public void setGroupid(Integer clusterid) {
+			setGroupid(clusterid.toString());
+		}
+
 		private Sample sample;
 
 		public String getGroupid() {
 			return groupid;
 		}
 
+		@Deprecated
 		public void setGroupid(String groupid) {
 			this.groupid = groupid;
 		}
