990e648236f853a1dd0e48a3d79adc1647c5f0cd
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2013-08-25 01:33:06 +0000

    acomodo la actualización de datos
    git-svn-id: https://svn.riouxsvn.com/bioplat04@938 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/editors/SurvCompExperimentExperimentsEditor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/editors/SurvCompExperimentExperimentsEditor.java
@@ -1,15 +1,18 @@
 package edu.unlp.medicine.bioplat.rcp.ui.experiment.editors;
 
+import java.util.List;
 import java.util.Observer;
 
 import org.eclipse.swt.widgets.Composite;
 
 import edu.unlp.medicine.bioplat.rcp.editor.AbstractEditorPart;
 import edu.unlp.medicine.bioplat.rcp.ui.biomarker.editors.GuiMaker;
-import edu.unlp.medicine.bioplat.rcp.ui.biomarker.editors.SurvCompHelper;
 import edu.unlp.medicine.bioplat.rcp.ui.biomarker.editors.SurvCompGUIMaker;
+import edu.unlp.medicine.bioplat.rcp.ui.biomarker.editors.SurvCompHelper;
 import edu.unlp.medicine.bioplat.rcp.ui.biomarker.editors.TableReferenceProvider2;
+import edu.unlp.medicine.bioplat.rcp.ui.utils.Provider;
 import edu.unlp.medicine.bioplat.rcp.ui.utils.tables.TableReference;
+import edu.unlp.medicine.domainLogic.framework.metasignatureGeneration.validation.SurvCompValidationResult;
 import edu.unlp.medicine.entity.experiment.Experiment;
 
 public class SurvCompExperimentExperimentsEditor extends AbstractEditorPart<Experiment> implements TableReferenceProvider2 {
@@ -34,7 +37,13 @@ public class SurvCompExperimentExperimentsEditor extends AbstractEditorPart<Expe
 
 	protected SurvCompHelper getHelper() {
 		if (helper == null)
-			helper = new SurvCompHelper(this, model().getSurvCompValidationResults());
+			helper = new SurvCompHelper(this, new Provider<List<SurvCompValidationResult>>() {
+
+				@Override
+				public List<SurvCompValidationResult> get() {
+					return model().getSurvCompValidationResults();
+				}
+			});
 		return helper;
 	}
 
