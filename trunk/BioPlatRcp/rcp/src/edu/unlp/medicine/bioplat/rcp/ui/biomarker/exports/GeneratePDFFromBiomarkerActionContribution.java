00feea90528ddf456cd633a7af9f01af97edd04c
jlichowski <jlichowski@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2015-05-26 02:41:31 +0000

    Se agrega la ventana de guardar como
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1022 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/exports/GeneratePDFFromBiomarkerActionContribution.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/exports/GeneratePDFFromBiomarkerActionContribution.java
@@ -1,6 +1,12 @@
 package edu.unlp.medicine.bioplat.rcp.ui.biomarker.exports;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
 
 import edu.unlp.medicine.bioplat.rcp.ui.entities.editors.contributors.AbstractActionContribution;
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
@@ -13,12 +19,17 @@ public class GeneratePDFFromBiomarkerActionContribution extends AbstractActionCo
 		@Override
 		public void run() {
 			PlatformUIUtils.findDisplay().asyncExec(new Runnable() {
-	
+				
 				@Override
 				public void run() {
+					FileDialog dialog = new FileDialog(PlatformUIUtils.findShell(), SWT.SAVE);
+					dialog.setFileName(model().getName()+".pdf");
 					
-					GeneratePDFFromBiomarkerCommand exportToPDF = new GeneratePDFFromBiomarkerCommand(model(), new HashMap<String, String>());
+					Map<String, String> properties = new HashMap<String, String>();
+					properties.put("targetFile", dialog.open());
+					GeneratePDFFromBiomarkerCommand exportToPDF = new GeneratePDFFromBiomarkerCommand(model(), properties);
 					exportToPDF.execute();
+					
 				}
 			});
 			
