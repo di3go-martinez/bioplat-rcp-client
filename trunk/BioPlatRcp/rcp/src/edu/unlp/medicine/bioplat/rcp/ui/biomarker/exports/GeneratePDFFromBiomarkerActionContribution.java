d3acb84016287f420c55e37a023b75a826e0361c
jlichowski <jlichowski@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2015-06-05 04:01:07 +0000

    Se agrega el dialogo de "Work in progress" al exportar a PDF. Se agrega una validacion para evitar nullpointer al cerrar el dialogo de seleccion de archivo a exportar
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1025 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/exports/GeneratePDFFromBiomarkerActionContribution.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/exports/GeneratePDFFromBiomarkerActionContribution.java
@@ -3,10 +3,9 @@ package edu.unlp.medicine.bioplat.rcp.ui.biomarker.exports;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Shell;
 
 import edu.unlp.medicine.bioplat.rcp.ui.entities.editors.contributors.AbstractActionContribution;
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
@@ -18,7 +17,8 @@ public class GeneratePDFFromBiomarkerActionContribution extends AbstractActionCo
 			
 		@Override
 		public void run() {
-			PlatformUIUtils.findDisplay().asyncExec(new Runnable() {
+			
+			inDisplay(new Runnable() {
 				
 				@Override
 				public void run() {
@@ -26,9 +26,13 @@ public class GeneratePDFFromBiomarkerActionContribution extends AbstractActionCo
 					dialog.setFileName(model().getName()+".pdf");
 					
 					Map<String, String> properties = new HashMap<String, String>();
-					properties.put("targetFile", dialog.open());
-					GeneratePDFFromBiomarkerCommand exportToPDF = new GeneratePDFFromBiomarkerCommand(model(), properties);
-					exportToPDF.execute();
+					String fileDir = dialog.open();
+					//Si el usuario cierra la ventana de exportacion, cerrarse.
+					if ( fileDir != null && !fileDir.isEmpty()) {
+						properties.put("targetFile", dialog.open());
+						GeneratePDFFromBiomarkerCommand exportToPDF = new GeneratePDFFromBiomarkerCommand(model(), properties);
+						exportToPDF.execute();
+					}
 					
 				}
 			});
