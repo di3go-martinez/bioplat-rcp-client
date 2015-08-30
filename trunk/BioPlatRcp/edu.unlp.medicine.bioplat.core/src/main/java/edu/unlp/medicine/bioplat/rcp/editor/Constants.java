83e2c5bfa72b26e25013716ba0c6fe4800136dab
davidnastasi <davidnastasi@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2015-08-30 23:28:07 +0000

    Quitar validaciones desde la vista
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1061 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/edu.unlp.medicine.bioplat.core/src/main/java/edu/unlp/medicine/bioplat/rcp/editor/Constants.java
+++ trunk/BioPlatRcp/edu.unlp.medicine.bioplat.core/src/main/java/edu/unlp/medicine/bioplat/rcp/editor/Constants.java
@@ -8,5 +8,7 @@ public interface Constants {
 	// TODO RENOMBRAR si siempre guarda el único gen seleccionado
 	String GENES = "genes key";
 	String SELECTED_GENES = "selected genes key";
+	String VALIDATIONS = "validations key";
+	String SELECTED_VALIDATIONS = "selected validations key";
 
 }
