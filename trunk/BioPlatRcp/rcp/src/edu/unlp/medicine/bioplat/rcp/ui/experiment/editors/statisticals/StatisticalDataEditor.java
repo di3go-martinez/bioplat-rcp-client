66e1251c9462a28e44eb7d99904566d7c8dfa2ce
davidnastasi <davidnastasi@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2015-09-20 21:58:58 +0000

    HeatMap
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1074 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/editors/statisticals/StatisticalDataEditor.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/editors/statisticals/StatisticalDataEditor.java
@@ -69,10 +69,9 @@ public class StatisticalDataEditor extends AbstractEditorPart<Validation> {
 		createExpandable(container, "ROC", new ROC(toolkit, model));
 		
 		createExpandable(container, "Kaplan-Meier", new SurvivalCurves(toolkit, model));
+		
+		createExpandable(container, "Heatmap", new Heatmap(toolkit, model));
 
 		
-		
-		
-
 	}
 }
