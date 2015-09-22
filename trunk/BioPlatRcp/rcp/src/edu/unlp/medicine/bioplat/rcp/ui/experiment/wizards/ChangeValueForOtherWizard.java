33d6841be5f87e971b8f6951b1450e6bdd9d9cd7
jlichowski <jlichowski@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2015-09-22 06:29:26 +0000

    Wizard para el cambio de valores más completo. Queda modificar el ExperimentClinicalData para que refresque los datos si se cambian los datos clinicos
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1076 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/wizards/ChangeValueForOtherWizard.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/wizards/ChangeValueForOtherWizard.java
@@ -1,34 +1,34 @@
 package edu.unlp.medicine.bioplat.rcp.ui.experiment.wizards;
 
-import java.awt.TextField;
-import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.jface.databinding.swt.SWTObservables;
-import org.eclipse.jface.databinding.viewers.ViewersObservables;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Text;
 
 import com.google.common.collect.Lists;
 
 import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.AbstractWizard;
 import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.WizardPageDescriptor;
-import edu.unlp.medicine.bioplat.rcp.ui.entities.wizards.databinding.UpdateStrategies;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
 import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
+import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 import edu.unlp.medicine.bioplat.rcp.utils.wizards.WizardModel;
 import edu.unlp.medicine.bioplat.rcp.widgets.Widgets;
-import edu.unlp.medicine.bioplat.rcp.widgets.wizards.Utils;
 import edu.unlp.medicine.domainLogic.ext.experimentCommands.logarithms.ENUM_LOGARITHM_BASE;
-import edu.unlp.medicine.domainLogic.ext.experimentCommands.logarithms.LogarithmToExpressionLevelsCommand;
 import edu.unlp.medicine.entity.experiment.AbstractExperiment;
 import edu.unlp.medicine.entity.experiment.Experiment;
 import edu.unlp.medicine.entity.experiment.Sample;
@@ -41,6 +41,11 @@ public class ChangeValueForOtherWizard extends AbstractWizard<Void> {
 //	private double newValue=0.0;
 	private String oldValue;
 	private String newValue;
+	private Button[] choice = null;
+	private boolean isClinicalData = false;
+	private String clinicalAttribute;
+	private boolean clinicalDataEnabled = true;
+	private Combo combo = null;
 	private AbstractExperiment experiment;
 
 	public ChangeValueForOtherWizard(AbstractExperiment abstractExperiment) {
@@ -79,6 +84,32 @@ public class ChangeValueForOtherWizard extends AbstractWizard<Void> {
 				dbc.bindValue(SWTObservables.observeText(newValueHolder, SWT.Modify), wizardModel().valueHolder("newValue"));
 				newValueHolder.setLayoutData(new GridData(100, 13));
 				
+				choice = new Button[2];
+
+				clinicalDataEnabled = experiment.getClinicalAttributeNames().size() > 0;
+				
+				choice[0] = new Button(c, SWT.RADIO);
+				choice[0].setSelection(true);
+				choice[0].setText("Replace expression data");
+				choice[0].addSelectionListener(generateSelectionListener());
+				
+				choice[1] = new Button(c, SWT.RADIO);
+//				choice[1].setSelection(false);
+				choice[1].setText("Replace clinical data");
+				choice[1].addSelectionListener(generateSelectionListener());
+				choice[1].setEnabled(clinicalDataEnabled);
+				
+				
+				new CLabel(c, SWT.BOLD).setText("Select the attribute to replace: ");
+				combo = new Combo(c, SWT.READ_ONLY);
+				combo.setItems(experiment.getClinicalAttributeNames().toArray(new String[0]));
+				combo.setEnabled(false);
+				combo.addSelectionListener(new SelectionAdapter() {
+					 public void widgetSelected(SelectionEvent e) {
+						 clinicalAttribute = combo.getText();
+					 }
+				});
+				
 				return c;
 			}
 		};
@@ -95,34 +126,38 @@ public class ChangeValueForOtherWizard extends AbstractWizard<Void> {
 	@Override
 	protected Void backgroundProcess(Monitor monitor) throws Exception {
 
-		try {
-			for (Sample s : experiment.getSamples())
-				for (Gene g : experiment.getGenes()) {
-					Double expr = experiment.getExpressionLevelForAGene(s, g);
-					if (expr == null || expr == Double.parseDouble(oldValue)) {
-						experiment.setExpressionLevelForAGene(s, g, Double.parseDouble(newValue));
-						addModification(s, g, Double.parseDouble(oldValue), Double.parseDouble(newValue));
+		long totalvaluesChanged = 0L;
+		if (isClinicalData){
+			try {
+				for(Sample s : experiment.getSamples()){
+//					for(String clinicalAttribute : experiment.getClinicalAttributeNames()){
+						if (experiment.getClinicalAttribute(s.getName(), clinicalAttribute).equalsIgnoreCase(oldValue)) {
+							experiment.setClinicalAttribute(s.getName(), clinicalAttribute, newValue);
+							totalvaluesChanged++;
+							MessageManager.INSTANCE.add(Message.info("The clinical attribute " +  oldValue + " in the sample " + s + " has changed. Old value: " + oldValue + ". New value: "+ newValue));
+						}
+//					}
 				}
-			}
-		} catch (Exception e) {
-			MessageManager.INSTANCE.add(Message.info("No expression data changed"));
-		}
-		
-		try {
-			for(Sample s : experiment.getSamples()){
-				for(String clinicalAttribute : experiment.getClinicalAttributeNames()){
-					if (experiment.getClinicalAttribute(s.getName(), clinicalAttribute).equalsIgnoreCase(oldValue)) {
-						experiment.setClinicalAttribute(s.getName(), clinicalAttribute, newValue);
-						experiment.getClinicalAttribute(s.getName(), clinicalAttribute);
-						MessageManager.INSTANCE.add(Message.info("The clinical attribute " +  oldValue + " in the sample " + s + " has changed. Old value: " + oldValue + ". New value: "+ newValue));
+			} catch (Exception e) {
+				MessageManager.INSTANCE.add(Message.info("No expression data changed"));
+			} 
+		} else {
+			try {
+				for (Sample s : experiment.getSamples())
+					for (Gene g : experiment.getGenes()) {
+						Double expr = experiment.getExpressionLevelForAGene(s, g);
+						if (expr == null || expr == Double.parseDouble(oldValue)) {
+							experiment.setExpressionLevelForAGene(s, g, Double.parseDouble(newValue));
+							addModification(s, g, Double.parseDouble(oldValue), Double.parseDouble(newValue));
+							totalvaluesChanged++;
+						}
 					}
-				}
+			} catch (Exception e) {
+				MessageManager.INSTANCE.add(Message.info("No expression data changed"));
 			}
-		} catch (Exception e) {
-			//No deberia suceder...
 		}
+		PlatformUIUtils.openInformation("Finished replacing values", totalvaluesChanged+" values changed. Old value: "+oldValue+". New value: "+newValue);
 		return null;
-
 	}
 
 	private ENUM_LOGARITHM_BASE lbase;
@@ -147,6 +182,17 @@ public class ChangeValueForOtherWizard extends AbstractWizard<Void> {
 	private void addModification(Sample s, Gene g, Double oldValue, Double newValue) {
 		MessageManager.INSTANCE.add(Message.info("The expression data of the gene " + g + " in the sample" + s + " has changed. Old value: " + oldValue + ". New value: " + newValue));
 	}
-
+	
+	private SelectionListener generateSelectionListener(){
+		SelectionListener selectionListener = new SelectionAdapter () {
+	         public void widgetSelected(SelectionEvent event) {
+	            Button button = ((Button) event.widget);
+	            isClinicalData = button.getText().equals("Replace clinical data");
+	            combo.setEnabled(isClinicalData);
+	         };
+	      };
+	    return selectionListener;
+	}
+	
 	
 }
