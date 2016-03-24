c2f028272a81e5790dd43023c7ccdb5571078d59
matiasbutti <matiasbutti@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2016-03-24 19:38:33 +0000

    git-svn-id: https://svn.riouxsvn.com/bioplat04@1136 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/startup/CheckConnectionToRemoteRServer.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/application/startup/CheckConnectionToRemoteRServer.java
@@ -5,23 +5,29 @@ import org.eclipse.ui.IStartup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.unlp.medicine.bioplat.rcp.ui.views.messages.Message;
+import edu.unlp.medicine.bioplat.rcp.ui.views.messages.MessageManager;
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 
 public class CheckConnectionToRemoteRServer implements IStartup {
 
 	protected static final Logger logger = LoggerFactory.getLogger(CheckConnectionToRemoteRServer.class);
+	public static String errorMessage = "Connection to Bioplat Server failed. \nAre you connected to internet? Please, check it and test the connection again using the CheckConnections Menu. \nIf you are not connected to Bioplat server you can not do any statistic analysis.";
 	
 	@Override
 	public void earlyStartup() {
 		try {
 			R4JConnection conn = R4JConnection.getInstance();
 			if (!conn.isConnected()){
-				PlatformUIUtils.openWarning("Could not connect to remote server", "There may be a problem with the connection to the internet or the server is under maintenance. Please, check your connection again from the CheckConnections Menu");
+				PlatformUIUtils.openWarning("Could not connect to remote server", errorMessage);
+				MessageManager.INSTANCE.add(Message.warn(errorMessage));
 			} else {
 				logger.info("Connection checked.");
+				MessageManager.INSTANCE.add(Message.info("You are succesfully connect to Bioplat Server"));
 			}
 		} catch (Exception e) {
-			PlatformUIUtils.openWarning("Could not connect to remote server", "There may be a problem with the connection to the internet or the server is under maintenance. Please, check your connection again from the CheckConnections Menu");
+			PlatformUIUtils.openWarning("Could not connect to remote server", errorMessage);
+			MessageManager.INSTANCE.add(Message.warn(errorMessage));
 		}
 	}
 
