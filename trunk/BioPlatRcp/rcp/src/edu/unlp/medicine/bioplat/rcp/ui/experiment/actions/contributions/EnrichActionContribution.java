72324d759be4980d0c6bf713144ec4d3707eba7f
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2017-08-19 16:14:12 +0000

    refactor
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1190 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/EnrichActionContribution.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/EnrichActionContribution.java
@@ -6,20 +6,27 @@ import edu.unlp.medicine.entity.biomarker.Biomarker;
 
 public class EnrichActionContribution extends AbstractActionContribution<Biomarker> {
 
-	@Override
-	public void run() {
-		PlatformUIUtils.findDisplay().syncExec(new Runnable() {
+  private static final String USE_INTERNAL_ENRICHR_BROWSER_PARAMETER = "INTERNAL_ENRICHR_BROWSER";
+  private static final String _DEFAULT_VALUE = "false";
 
-			@Override
-			public void run() {
-				if ("true".equals(System.getProperty("INTERNAL_ENRICHR_BROWSER", "false")))
-					// por ahora no anda...
-					new EnrichrDialog().openWith(model());
-				else
-					new ExternalEnrichR().openWith(model());
-				
-				
-			}
-		});
-	}
+  @Override
+  public void run() {
+    PlatformUIUtils.findDisplay().syncExec(new Runnable() {
+
+      @Override
+      public void run() {
+        if (showEmbeded())
+          //TODO por ahora no anda con el internal, hacer pruebas en distintos browsers
+          new EnrichrDialog().openWith(model());
+        else
+          new ExternalEnrichR().openWith(model());
+      }
+
+      private boolean showEmbeded() {
+        String value = System.getProperty(USE_INTERNAL_ENRICHR_BROWSER_PARAMETER, _DEFAULT_VALUE);
+        return "true".equals(value);
+      }
+    });
+  }
+
 }
