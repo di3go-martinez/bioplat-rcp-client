b186b3f1c69ea883047f122556c5668817c3cce3
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2017-08-19 16:18:27 +0000

    javadoc y refactor
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1192 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/ExternalEnrichR.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/actions/contributions/ExternalEnrichR.java
@@ -8,38 +8,50 @@ import java.net.URL;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.unlp.medicine.entity.biomarker.Biomarker;
 
+/**
+ * 
+ * 
+ * Abre la integración con EnrichR en el browser configurado del sistema operativo
+ * 
+ * @author diego
+ * @see EnrichActionContribution
+ *
+ */
 public class ExternalEnrichR {
 
-	public void openWith(Biomarker model) {
-		String html = EnrichrDialog.buildEnrichRHtml(model.getGenes());
-		try {
-			File f = File.createTempFile("tmp", ".html");
-			f.deleteOnExit();
-			new FileWriter(f).append(html).flush();
+  public void openWith(Biomarker model) {
+    String html = EnrichrDialog.buildEnrichRHtml(model.getGenes());
+    try {
+      File f = File.createTempFile("tmp", ".html");
+      f.deleteOnExit();
+      FileWriter fileWriter = new FileWriter(f);
+      fileWriter.append(html);
+      fileWriter.close();
 
-			openSystemBrowser("file:///" + f.getAbsolutePath());
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+      openSystemBrowser("file:///" + f.getAbsolutePath());
 
-	}
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-	public static IWebBrowser openSystemBrowser(String url) {
-		try {
-			IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
-			IWebBrowser browser = browserSupport.getExternalBrowser();
-			browser.openURL(new URL(url));
-			return browser;
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
-			
-		}
-	}
+  }
+
+  public static void openSystemBrowser(String url) {
+    try {
+      IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
+      IWebBrowser browser = browserSupport.getExternalBrowser();
+      browser.openURL(new URL(url));
+    } catch (Exception e) {
+      logger.error("Error openning the external web browser, with url: "+url, e);
+    }
+  }
+  
+  private static final Logger logger = LoggerFactory.getLogger(ExternalEnrichR.class);
 
 }
