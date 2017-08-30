33ca9ada939f537c7b6b66704b20a9d4b518f451
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2017-08-30 18:55:49 +0000

    organize imports
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1218 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/org.bioplat.multiomics/src/org/bioplat/multiomics/handler/OpenWebPortHandler.java
+++ trunk/BioPlatRcp/org.bioplat.multiomics/src/org/bioplat/multiomics/handler/OpenWebPortHandler.java
@@ -4,7 +4,6 @@ import org.bioplat.multiomics.MultiomicsWebPort;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IViewPart;
 
 import edu.unlp.medicine.bioplat.rcp.utils.PlatformUIUtils;
 
