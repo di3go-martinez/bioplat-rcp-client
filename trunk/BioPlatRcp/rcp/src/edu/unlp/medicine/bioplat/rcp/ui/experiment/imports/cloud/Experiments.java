638dea1dca4aee6dff59a0a652ef3897ed7e43de
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2017-10-21 17:10:44 +0000

    Nuevos métodos email(author), arnmPlatform() con valores por default
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1319 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/imports/cloud/Experiments.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/experiment/imports/cloud/Experiments.java
@@ -1,7 +1,6 @@
 package edu.unlp.medicine.bioplat.rcp.ui.experiment.imports.cloud;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
@@ -15,6 +14,7 @@ import com.google.common.collect.Sets;
 
 import edu.unlp.medicine.domainLogic.framework.MetaPlat;
 import edu.unlp.medicine.domainLogic.framework.exceptions.GeneNotFoundByIdException;
+import edu.unlp.medicine.entity.arnmPlatform.ARNmPlatform;
 import edu.unlp.medicine.entity.experiment.Experiment;
 import edu.unlp.medicine.entity.experiment.Sample;
 import edu.unlp.medicine.entity.experiment.collapse.strategy.media.MediaCollapseStrategy;
@@ -40,12 +40,27 @@ public enum Experiments {
 			samples.put(sample.getName(), sample);// TODO confirmar si el nombre es la clave del mapa que espera el
 													// constructor
 
-		Experiment e = new Experiment(dataset.getName(), dataset.getAuthor(), null, null, samples,
-				samplesExpressionProfiles);
+		Experiment e = new Experiment(dataset.getName(), dataset.getAuthor(), email(dataset.getAuthor()), arnmPlatform(),
+				samples, samplesExpressionProfiles);
 
 		setClinicalAttributes(e, dataset);
 
 		return e;
+	}
+
+	private ARNmPlatform arnmPlatform() {
+		// TODO de donde se saca?
+		return new ARNmPlatform();
+	}
+
+	private String email(String author) {
+	    // TODO de donde se saca?
+	    return author.replace(" ", "_")+"@mail.com";
+	}
+
+	// TODO parametrizar? el dataset yalo tendría que tener?
+	private MediaCollapseStrategy collapseStrategy() {
+		return new MediaCollapseStrategy();
 	}
 
 	private void setClinicalAttributes(Experiment e, DatasetDTO dataset) {
@@ -56,17 +71,20 @@ public enum Experiments {
 
 	private Map<Sample, TObjectDoubleHashMap<Gene>> samplesExpressionProfiles(final DatasetDTO dataset) {
 		final Map<Sample, TObjectDoubleHashMap<Gene>> samplesExpressionProfiles = Maps.newHashMap();
-
+		
 		for (SampleDTO sampledto : samples(dataset)) {
-
-			Sample samplemodel = new Sample(sampledto.getSampleID(), collapseStrategy());
-
-			for (Gene gene : genes(dataset, sampledto))
-				samplemodel.setGeneExpressionLevel(gene, findExpression(sampledto, gene));
-
-			samplesExpressionProfiles.put(samplemodel, samplemodel.collapseExpressionData());
+			Sample sample = createSample(dataset, sampledto);
+			samplesExpressionProfiles.put(sample, sample.collapseExpressionData());
 		}
 		return samplesExpressionProfiles;
+	}
+
+	private Sample createSample(final DatasetDTO dataset, SampleDTO sampledto) {
+		Sample samplemodel = new Sample(sampledto.getSampleID(), collapseStrategy());
+
+		for (Gene gene : genes(dataset, sampledto))
+			samplemodel.setGeneExpressionLevel(gene, findExpression(sampledto, gene));
+		return samplemodel;
 	}
 
 	private Double findExpression(SampleDTO sampledto, Gene gene) {
@@ -77,32 +95,27 @@ public enum Experiments {
 	}
 
 	private Set<Gene> genes(DatasetDTO dataset, SampleDTO sampledto) {
-		Set<Gene> genes  = Sets.newHashSet();
-		Map<String, Number> expressions =  sampledto.getMolecularsData().get(MolecularDataType.mrna);
-		//TODO confirmar que es el genename
-		for (String genename: expressions.keySet()) {
+		Set<Gene> genes = Sets.newHashSet();
+		Map<String, Number> expressions = sampledto.getMolecularsData().get(MolecularDataType.mrna);
+		// TODO confirmar que es el genename
+		for (String genename : expressions.keySet()) 
 			try {
 				genes.add(MetaPlat.getInstance().getGeneByName(genename));
 			} catch (GeneNotFoundByIdException re) {
 				logger.error("Gene not found", re);
 			}
-		}
-		return genes;
 		
+		return genes;
+
 	}
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(Experiments.class);
 
-	// TODO parametrizar
-	private MediaCollapseStrategy collapseStrategy() {
-		return new MediaCollapseStrategy();
-	}
-
 	/**
-	 * @return todos los samples del dataset
+	 * @return todos los samples del dataset.  
+	 *
 	 */
-	// TODO revisar, este método debería estar en DatasetDTO
-	// TODO getSamples debería ser un set de samples con todos los samples
+	//FIXME reemplazar con dataset.getAllSamples(). pedir cambio en la api de bioplat-cloud...
 	private Collection<SampleDTO> samples(final DatasetDTO dataset) {
 		Collection<SampleDTO> samplesdto = Sets.newHashSet();
 		for (Collection<SampleDTO> part : dataset.getSamples().values())
