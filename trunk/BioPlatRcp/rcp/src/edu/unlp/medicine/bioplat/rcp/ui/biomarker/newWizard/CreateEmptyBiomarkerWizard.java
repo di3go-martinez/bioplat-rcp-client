7747be73a91efb150fdbdecc9d0d07fb774a533b
diegom <diegom@2787e032-bcb8-4b3a-9c9c-088c20af2cfc>
2017-11-11 02:39:35 +0000

    Organize imports
    git-svn-id: https://svn.riouxsvn.com/bioplat04@1334 2787e032-bcb8-4b3a-9c9c-088c20af2cfc

--- trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/newWizard/CreateEmptyBiomarkerWizard.java
+++ trunk/BioPlatRcp/rcp/src/edu/unlp/medicine/bioplat/rcp/ui/biomarker/newWizard/CreateEmptyBiomarkerWizard.java
@@ -10,7 +10,6 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
